5f97fe8506a4851dca8e90f25508abb335f7d1bb
howardwu <howardwu@berkeley.edu>
2021-01-28 22:46:19 -0800

    Migrates to bencher for benches

--- benches/account.rs
+++ benches/account.rs
@@ -16,31 +16,32 @@
 
 use aleo::account::{Address, PrivateKey};
 
-use criterion::{criterion_group, criterion_main, Criterion};
+#[macro_use]
+extern crate bencher;
+
+use bencher::Bencher;
 use rand::SeedableRng;
 use rand_chacha::ChaChaRng;
 
-fn account_bench(c: &mut Criterion) {
-    let mut group = c.benchmark_group("account");
-    group.sample_size(20);
+pub const SEED: u64 = 1231275789u64;
 
-    let rng = &mut ChaChaRng::seed_from_u64(1231275789u64);
+fn account_private_key(bench: &mut Bencher) {
+    let rng = &mut ChaChaRng::seed_from_u64(SEED);
 
-    group.bench_function("private_key", |b| {
-        b.iter(|| {
-            let _private_key = PrivateKey::new(rng).unwrap();
-        });
-    });
-
-    let private_key = PrivateKey::new(rng).unwrap();
-
-    group.bench_function("address", |b| {
-        b.iter(|| {
-            let _address = Address::from(&private_key).unwrap();
-        });
+    bench.iter(|| {
+        let _private_key = PrivateKey::new(rng).unwrap();
     });
 }
 
-criterion_group!(benches, account_bench);
+fn account_address(bench: &mut Bencher) {
+    let rng = &mut ChaChaRng::seed_from_u64(SEED);
 
-criterion_main!(benches);
+    let private_key = PrivateKey::new(rng).unwrap();
+
+    bench.iter(|| {
+        let _address = Address::from(&private_key).unwrap();
+    });
+}
+
+benchmark_group!(account, account_private_key, account_address);
+benchmark_main!(account);
