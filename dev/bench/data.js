e896c93edb0d2cb790056e05095232ae592c46b5
github-action-benchmark <github@users.noreply.github.com>
2021-02-04 07:03:08 +0000

    add Aleo SDK Benchmarks (cargo) benchmark result for dd062a1246df831dde69c286904d73c42ae17742

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1612421732082,
+  "lastUpdate": 1612422188506,
   "repoUrl": "https://github.com/AleoHQ/aleo",
   "entries": {
     "Aleo SDK Benchmarks": [
@@ -35,6 +35,42 @@ window.BENCHMARK_DATA = {
             "name": "account_private_key",
             "value": 711758682,
             "range": "± 27312297",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "howardwu@berkeley.edu",
+            "name": "howardwu",
+            "username": "howardwu"
+          },
+          "committer": {
+            "email": "howardwu@berkeley.edu",
+            "name": "howardwu",
+            "username": "howardwu"
+          },
+          "distinct": true,
+          "id": "dd062a1246df831dde69c286904d73c42ae17742",
+          "message": "Add exclusion",
+          "timestamp": "2021-02-03T22:51:52-08:00",
+          "tree_id": "abd635ac43c2ee5a21c632070e893f16d20abe67",
+          "url": "https://github.com/AleoHQ/aleo/commit/dd062a1246df831dde69c286904d73c42ae17742"
+        },
+        "date": 1612422186928,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "account_address",
+            "value": 71833989,
+            "range": "± 6399432",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "account_private_key",
+            "value": 817455858,
+            "range": "± 61938389",
             "unit": "ns/iter"
           }
         ]
