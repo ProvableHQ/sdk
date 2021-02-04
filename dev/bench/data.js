5934eedc3345fc2237f13248a3d302ec30533523
github-action-benchmark <github@users.noreply.github.com>
2021-02-04 05:25:22 +0000

    add Aleo SDK Benchmarks (cargo) benchmark result for 975c7c453850552247851b8e363fd8bd3e776d60

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1612416322353,
+  "repoUrl": "https://github.com/AleoHQ/aleo",
+  "entries": {
+    "Aleo SDK Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "9260812+howardwu@users.noreply.github.com",
+            "name": "Howard Wu",
+            "username": "howardwu"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "975c7c453850552247851b8e363fd8bd3e776d60",
+          "message": "Merge pull request #17 from AleoHQ/fix/ci\n\nFix codecov and introduce more testing",
+          "timestamp": "2021-02-03T21:07:47-08:00",
+          "tree_id": "2752c07626550502c8ba5e945755dcb29c55588c",
+          "url": "https://github.com/AleoHQ/aleo/commit/975c7c453850552247851b8e363fd8bd3e776d60"
+        },
+        "date": 1612416321005,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "account_address",
+            "value": 66789362,
+            "range": "± 3837038",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "account_private_key",
+            "value": 776215740,
+            "range": "± 22724221",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
