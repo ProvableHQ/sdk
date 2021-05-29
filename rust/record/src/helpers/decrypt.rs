31ce09493311b7f789c3d9ea8c806e160c714ea8
howardwu <howard@aleo.org>
2021-05-28 19:34:21 -0700

    Simplify decrypt method

--- rust/record/src/helpers/decrypt.rs
+++ rust/record/src/helpers/decrypt.rs
@@ -27,15 +27,12 @@ impl Decrypt {
     pub fn decrypt(view_key: &ViewKey, encrypted_record: &[u8]) -> Result<Record, RecordError> {
         let system_parameters = SystemParameters::<Components>::load()?;
 
-        let record_native = RecordEncryption::<Components>::decrypt_record(
+        let record = RecordEncryption::<Components>::decrypt_record(
             &system_parameters,
             &view_key.view_key,
             &FromBytes::read(&encrypted_record[..])?,
         )?;
 
-        let mut record_bytes = vec![];
-        record_native.write(&mut record_bytes)?;
-
-        Ok(Record::read(&record_bytes[..])?)
+        Ok(Record::read(&to_bytes![record]?[..])?)
     }
 }
