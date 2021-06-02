8dc8e8e104dcad72923d40be79737c03cb3e58de
collin <16715212+collinc97@users.noreply.github.com>
2021-06-02 12:39:41 -0700

    add record builder unit test, remove duplicated test

--- rust/record/src/tests.rs
+++ rust/record/src/tests.rs
@@ -13,7 +13,8 @@
 
 // You should have received a copy of the GNU General Public License
 // along with the Aleo library. If not, see <https://www.gnu.org/licenses/>.
-use crate::Record;
+use crate::{Commitment, CommitmentRandomness, Record, SerialNumberNonce};
+use aleo_account::{Address, PrivateKey};
 
 use snarkvm_algorithms::traits::CRH;
 use snarkvm_dpc::testnet1::{
@@ -22,10 +23,18 @@ use snarkvm_dpc::testnet1::{
     NoopProgramSNARKParameters,
     SystemParameters,
 };
-use snarkvm_utilities::{to_bytes, ToBytes};
+use snarkvm_utilities::{to_bytes, FromBytes, ToBytes};
 
-use rand::SeedableRng;
+use rand::{Rng, SeedableRng};
 use rand_chacha::ChaChaRng;
+use std::str::FromStr;
+
+const TEST_RECORD_OWNER: &str = "aleo1faksgtpmculyzt6tgaq26fe4fgdjtwualyljjvfn2q6k42ydegzspfz9uh";
+const TEST_RECORD_VALUE: u64 = 100;
+const TEST_RECORD_PAYLOAD: &str = "e456ab4252bd295128ee51efeb4835a9f6f29f818d13021802df91079599ab3a";
+const TEST_RECORD_SERIAL_NUMBER_NONCE: &str = "89357d5fedcb36fa5662bd902689739eafc10e30e84c86648d6a5ec0be52cc00";
+const TEST_RECORD_COMMITMENT_RANDOMNESS: &str = "aa83a5428e0827c262d2ccad4be75fc1d01ebd0f9a5c0cc2d7a6557cb8664101";
+const TEST_RECORD_COMMITMENT: &str = "9cb6e5813f7008058906868de82c27764cb058aa361d0224f76877898127a304";
 
 #[test]
 fn test_build_dummy_record() {
@@ -63,4 +72,65 @@ fn test_build_dummy_record() {
 
     // Check death_program_id: noop_program_id
     assert_eq!(dummy_record.death_program_id, noop_program_id);
+}
+
+#[test]
+fn test_build_record() {
+    // Load system parameters for the ledger, commitment schemes, CRH, and the
+    // "always-accept" program.
+    let system_parameters = SystemParameters::<Components>::load().unwrap();
+    let program_snark_pp = NoopProgramSNARKParameters::<Components>::load().unwrap();
+
+    let program_snark_vk_bytes = to_bytes![
+        ProgramVerificationKeyCRH::hash(
+            &system_parameters.program_verification_key_crh,
+            &to_bytes![program_snark_pp.verification_key].unwrap()
+        )
+        .unwrap()
+    ]
+    .unwrap();
+
+    let owner = Address::from_str(TEST_RECORD_OWNER).unwrap();
+
+    let value = TEST_RECORD_VALUE;
+
+    let payload_bytes = hex::decode(TEST_RECORD_PAYLOAD).unwrap();
+    let payload = Payload::read(&payload_bytes[..]).unwrap();
+
+    let serial_number_nonce_bytes = hex::decode(TEST_RECORD_SERIAL_NUMBER_NONCE).unwrap();
+    let serial_number_nonce = SerialNumberNonce::read(&serial_number_nonce_bytes[..]).unwrap();
+
+    let commitment_randomness_bytes = hex::decode(TEST_RECORD_COMMITMENT_RANDOMNESS).unwrap();
+    let commitment_randomness = CommitmentRandomness::read(&commitment_randomness_bytes[..]).unwrap();
+
+    let commitment_bytes = hex::decode(TEST_RECORD_COMMITMENT).unwrap();
+    let commitment = Commitment::read(&commitment_bytes[..]).unwrap();
+
+    // Generate new record from randomness
+    let given_record = Record::new()
+        .owner(owner.clone())
+        .value(value)
+        .payload(payload.clone())
+        .birth_program_id(program_snark_vk_bytes.clone())
+        .death_program_id(program_snark_vk_bytes.clone())
+        .serial_number_nonce(serial_number_nonce)
+        .commitment_randomness(commitment_randomness)
+        .build()
+        .unwrap();
+
+    assert_eq!(given_record.owner, owner);
+
+    assert_eq!(given_record.value, value);
+
+    assert_eq!(given_record.payload, payload);
+
+    assert_eq!(given_record.birth_program_id, program_snark_vk_bytes);
+
+    assert_eq!(given_record.death_program_id, program_snark_vk_bytes);
+
+    assert_eq!(given_record.serial_number_nonce, serial_number_nonce);
+
+    assert_eq!(given_record.commitment_randomness, commitment_randomness);
+
+    assert_eq!(given_record.commitment, commitment);
 }
