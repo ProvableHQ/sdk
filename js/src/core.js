291677786588b148f02130c8d8e1d0e30a1d6900
collin <16715212+collinc97@users.noreply.github.com>
2021-06-29 10:46:21 -0700

    add js methods for record and transactions

--- js/src/core.js
+++ js/src/core.js
@@ -18,6 +18,119 @@ class Account {
     }
 }
 
-module.exports.Account = Account;
+class Record {
+    constructor() {
+        this.record = new aleo.Record();
+    }
 
+    to_string() {
+        return this.record.to_string();
+    }
+
+    owner() {
+        return this.record.owner();
+    }
+
+    value() {
+        return this.record.value();
+    }
+
+    is_dummy() {
+        return this.record.is_dummy();
+    }
+
+    payload() {
+        return this.record.payload();
+    }
+
+    birth_program_id() {
+        return this.record.birth_program_id();
+    }
+
+    death_program_id() {
+        return this.record.death_program_id();
+    }
+
+    serial_number_nonce() {
+        return this.record.serial_number_nonce();
+    }
+
+    commitment() {
+        return this.record.commitment();
+    }
+
+    commitment_randomness() {
+        return this.record.commitment_randomness();
+    }
+}
+
+class Transaction {
+    constructor() {
+        this.transaction = new aleo.Transaction();
+    }
+
+    to_string() {
+        return this.transaction.to_string();
+    }
+
+    transaction_id() {
+        return this.transaction.transaction_id();
+    }
+
+    network_id() {
+        return this.transaction.network_id();
+    }
+
+    ledger_digest() {
+        return this.transaction.ledger_digest();
+    }
+
+    inner_circuit_id() {
+        return this.transaction.inner_circuit_id();
+    }
+
+    old_serial_numbers() {
+        return this.transaction.old_serial_numbers();
+    }
+
+    new_commitments() {
+        return this.transaction.new_commitments();
+    }
+
+    program_commitment() {
+        return this.transaction.program_commitment();
+    }
+
+    local_data_root() {
+        return this.transaction.local_data_root();
+    }
+
+    value_balance() {
+        return this.transaction.value_balance();
+    }
+
+    encrypted_records() {
+        return this.transaction.encrypted_records();
+    }
+
+    memorandum() {
+        return this.transaction.memorandum();
+    }
+
+    size() {
+        return this.transaction.size();
+    }
+}
+
+module.exports.Account = Account;
+module.exports.Record = Record;
+module.exports.Transaction = Transaction;
+
+console.log("New Account:");
 console.log(new Account().address());
+console.log("");
+
+console.log("New Record:");
+console.log(new Record().to_string());
+console.log("");
+
