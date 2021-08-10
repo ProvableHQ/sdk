7557de1917c734c58a52fba7fb4f56174f167078
collin <16715212+collinc97@users.noreply.github.com>
2021-08-10 15:00:10 -0700

    refactor network module to use feat/dpc-state

---
+++ rust/network/src/testnet2.rs
@@ -0,0 +1,27 @@
+// Copyright (C) 2019-2021 Aleo Systems Inc.
+// This file is part of the Aleo library.
+
+// The Aleo library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// The Aleo library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with the Aleo library. If not, see <https://www.gnu.org/licenses/>.
+
+use crate::Network;
+use snarkvm_dpc::testnet2::Testnet2Parameters;
+
+/// The testnet2 network
+pub struct Testnet2;
+
+impl Network for Testnet2 {
+    type Parameters = Testnet2Parameters;
+
+    const ID: u8 = 2;
+}
