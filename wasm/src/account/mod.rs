e492bd47cd97953d284f5eb413305b7d03215519
collin <16715212+collinc97@users.noreply.github.com>
2022-08-11 17:19:09 -0700

    update snarkvm rev and wasm code

--- wasm/src/account/mod.rs
+++ wasm/src/account/mod.rs
@@ -14,5 +14,5 @@
 // You should have received a copy of the GNU General Public License
 // along with the Aleo library. If not, see <https://www.gnu.org/licenses/>.
 
-pub mod account;
-pub use account::*;
+pub mod address;
+pub use address::*;
