a88f4c2fc81da6c2bf3018425a8718bbdf8250a1
Howard Wu <9260812+howardwu@users.noreply.github.com>
2022-10-06 11:33:36 -0700

    Update view key

--- wasm/src/account/view_key.rs
+++ wasm/src/account/view_key.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with the Aleo library. If not, see <https://www.gnu.org/licenses/>.
 
-use aleo_account::{PrivateKey, RecordCiphertext, ViewKey as ViewKeyNative};
+use aleo_account::{PrivateKey, Record, ViewKey as ViewKeyNative};
 
 use std::{convert::TryFrom, str::FromStr};
 use wasm_bindgen::prelude::*;
@@ -47,10 +47,10 @@ impl ViewKey {
 
     #[wasm_bindgen]
     pub fn decrypt(&self, ciphertext: &str) -> Result<String, String> {
-        let ciphertext = RecordCiphertext::from_str(ciphertext).map_err(|error| error.to_string())?;
+        let ciphertext = Record::from_str(ciphertext).map_err(|error| error.to_string())?;
         match ciphertext.decrypt(&self.view_key) {
-            Ok(plaintext) => Ok(serde_json::to_string(&plaintext).map_err(|error| error.to_string())?),
-            Err(_) => Err("Incorrect view key".to_string()),
+            Ok(plaintext) => Ok(plaintext.to_string()),
+            Err(error) => Err(error.to_string()),
         }
     }
 }
@@ -61,34 +61,35 @@ mod tests {
 
     use wasm_bindgen_test::*;
 
-    const CORRECT_VIEW_KEY: &str = "AViewKey1dcqVNvMqYGoVtaQJW1YnmH23XABeeQTq9d6XmYUmo7CW";
-    const INCORRECT_VIEW_KEY: &str = "AViewKey1i3fn5SECcVBtQMCVtTPSvdApoMYmg3ToJfNDfgHJAuoD";
+    const OWNER_PLAINTEXT: &str = r"{
+  owner: aleo1snwe5h89dv6hv2q2pl3v8l9cweeuwrgejmlnwza6ndacygznlu9sjt8pgv.private,
+  gates: 1u64.private,
+  _nonce: 4447510634654730534613001085815220248957154008834207042015711498717088580021group.public
+}";
+    const OWNER_CIPHERTEXT: &str = "record1qyqspplg2ud9gguy8ud9wjmee3cf2vztxcjxe2ernf8m7ru5wvsqkdqxqyqsq7y540qmemqx3675pufewwmywsudzrpstjx3fd38c6d8uz4r4mgpqqqt2q2jjczxp2y6986zdqz3mr5jmhggmge3exc72vgw2kgr4gea2zgzhrz8q";
+    const OWNER_VIEW_KEY: &str = "AViewKey1dcqVNvMqYGoVtaQJW1YnmH23XABeeQTq9d6XmYUmo7CW";
+    const NON_OWNER_VIEW_KEY: &str = "AViewKey1i3fn5SECcVBtQMCVtTPSvdApoMYmg3ToJfNDfgHJAuoD";
 
     #[wasm_bindgen_test]
     pub fn test_from_private_key() {
         let given_private_key = "APrivateKey1zkp4RyQ8Utj7aRcJgPQGEok8RMzWwUZzBhhgX6rhmBT8dcP";
         let given_view_key = "AViewKey1i3fn5SECcVBtQMCVtTPSvdApoMYmg3ToJfNDfgHJAuoD";
-
         let view_key = ViewKey::from_private_key(given_private_key);
-
         assert_eq!(given_view_key, view_key.to_string());
     }
 
     #[wasm_bindgen_test]
     pub fn test_decrypt_success() {
-        let ciphertext = "record1qyqspplg2ud9gguy8ud9wjmee3cf2vztxcjxe2ernf8m7ru5wvsqkdqxqyqsq7y540qmemqx3675pufewwmywsudzrpstjx3fd38c6d8uz4r4mgpqqqt2q2jjczxp2y6986zdqz3mr5jmhggmge3exc72vgw2kgr4gea2zgzhrz8q";
-        let view_key = ViewKey::from_string(CORRECT_VIEW_KEY);
-        let plaintext = view_key.decrypt(ciphertext);
-        let expected_plaintext = "{\"owner\":\"aleo1snwe5h89dv6hv2q2pl3v8l9cweeuwrgejmlnwza6ndacygznlu9sjt8pgv.private\",\"gates\":\"1u64.private\",\"data\":{},\"_nonce\":\"4447510634654730534613001085815220248957154008834207042015711498717088580021group\"}";
+        let view_key = ViewKey::from_string(OWNER_VIEW_KEY);
+        let plaintext = view_key.decrypt(OWNER_CIPHERTEXT);
         assert!(plaintext.is_ok());
-        assert_eq!(expected_plaintext, plaintext.unwrap())
+        assert_eq!(OWNER_PLAINTEXT, plaintext.unwrap())
     }
 
     #[wasm_bindgen_test]
     pub fn test_decrypt_fails() {
-        let ciphertext = "record1qyqspplg2ud9gguy8ud9wjmee3cf2vztxcjxe2ernf8m7ru5wvsqkdqxqyqsq7y540qmemqx3675pufewwmywsudzrpstjx3fd38c6d8uz4r4mgpqqqt2q2jjczxp2y6986zdqz3mr5jmhggmge3exc72vgw2kgr4gea2zgzhrz8q";
-        let incorrect_private_key = ViewKey::from_string(INCORRECT_VIEW_KEY);
-        let plaintext = incorrect_private_key.decrypt(ciphertext);
+        let incorrect_private_key = ViewKey::from_string(NON_OWNER_VIEW_KEY);
+        let plaintext = incorrect_private_key.decrypt(OWNER_CIPHERTEXT);
         assert!(plaintext.is_err());
     }
 }
