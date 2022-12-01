2f46d2ba27293196c9cd0247d2410e0f2b2a8c33
github-action-benchmark <github@users.noreply.github.com>
2022-12-01 22:50:52 +0000

    add Aleo SDK Benchmarks (cargo) benchmark result for c8544a7961cb29454efdaa2e4de89d1bea788fe0

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1669935052684,
+  "repoUrl": "https://github.com/AleoHQ/aleo",
+  "entries": {
+    "Aleo SDK Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "collin@aleo.org",
+            "name": "Collin Chin",
+            "username": "collinc97"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c8544a7961cb29454efdaa2e4de89d1bea788fe0",
+          "message": "bump snarkvm v0.9.9 (#424)",
+          "timestamp": "2022-12-01T14:37:31-08:00",
+          "tree_id": "3f589968c996cbcc34e05904864ddf3207dd1e1e",
+          "url": "https://github.com/AleoHQ/aleo/commit/c8544a7961cb29454efdaa2e4de89d1bea788fe0"
+        },
+        "date": 1669935049267,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "testnet3_address_from_private_key",
+            "value": 236688,
+            "range": "± 29789",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "testnet3_private_key_new",
+            "value": 92404,
+            "range": "± 10391",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
