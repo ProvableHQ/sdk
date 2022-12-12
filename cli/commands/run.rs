13c3e2dfb27040a6ce4b366d00d82788ec6a99b5
Collin Chin <collin@aleo.org>
2022-12-12 15:38:57 -0800

    Feature - log program metrics (#433)
    * wip output metrics from package run
    
    * improve constraint formatting and function counter
    
    * clippy
    
    * update snarkvm tag v0.9.11

--- cli/commands/run.rs
+++ cli/commands/run.rs
@@ -24,6 +24,9 @@ use anyhow::Result;
 use clap::Parser;
 use colored::Colorize;
 use core::str::FromStr;
+use std::collections::HashMap;
+
+pub const LOCALE: &num_format::Locale = &num_format::Locale::en;
 
 /// Executes an Aleo program function.
 #[derive(Debug, Parser)]
@@ -56,13 +59,48 @@ impl Run {
         let rng = &mut rand::thread_rng();
 
         // Execute the request.
-        let (response, _transition, _inclusion) = package.run::<Aleo, _>(
+        let (response, _transition, _inclusion, metrics) = package.run::<Aleo, _>(
             self.endpoint,
             package.manifest_file().development_private_key(),
             self.function,
             &self.inputs,
             rng,
         )?;
+
+        // Count the number of times a function is called.
+        let mut program_frequency = HashMap::<String, usize>::new();
+        for metric in metrics.iter() {
+            // Prepare the function name string.
+            let function_name_string = format!("'{}/{}'", metric.program_id, metric.function_name).bold();
+
+            // Prepare the function constraints string
+            let function_constraints_string = format!(
+                "{function_name_string} - {} constraints",
+                metric.num_function_constraints.to_formatted_string(LOCALE)
+            );
+
+            // Increment the counter for the function call.
+            match program_frequency.get_mut(&function_constraints_string) {
+                Some(counter) => *counter += 1,
+                None => {
+                    let _ = program_frequency.insert(function_constraints_string, 1);
+                }
+            }
+        }
+
+        // Log the metrics.
+        use num_format::ToFormattedString;
+
+        println!("\n⛓  Constraints\n");
+        for (function_constraints, counter) in program_frequency {
+            // Log the constraints
+            let counter_string = match counter {
+                1 => "(called 1 time)".to_string().dimmed(),
+                counter => format!("(called {} times)", counter).dimmed(),
+            };
+
+            println!(" •  {function_constraints} {counter_string}",)
+        }
 
         // Log the outputs.
         match response.outputs().len() {
