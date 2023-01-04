1b1c770cfbf66a7c479dee41d5cc1cf49031fb1d
github-action-benchmark <github@users.noreply.github.com>
2023-01-04 02:24:24 +0000

    add Aleo SDK Benchmarks (cargo) benchmark result for d838866f7eb59108df83915160d0c9f1a6fa970b

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1672799064940,
+  "repoUrl": "https://github.com/AleoHQ/aleo",
+  "entries": {
+    "Aleo SDK Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "collin@aleo.org",
+            "name": "Collin Chin",
+            "username": "collinc97"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d838866f7eb59108df83915160d0c9f1a6fa970b",
+          "message": "Update dependencies (#448)\n\n* update dependencies\r\n\r\n* fix clippy",
+          "timestamp": "2023-01-03T18:13:03-08:00",
+          "tree_id": "afb208bf99e3e258d963b48cd52fd98a76e2071e",
+          "url": "https://github.com/AleoHQ/aleo/commit/d838866f7eb59108df83915160d0c9f1a6fa970b"
+        },
+        "date": 1672799061092,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "testnet3_address_from_private_key",
+            "value": 263461,
+            "range": "± 72458",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "testnet3_private_key_new",
+            "value": 113827,
+            "range": "± 15844",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
