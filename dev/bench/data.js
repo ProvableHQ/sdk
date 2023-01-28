68866e4dfa825e7999bf548e841e8c7a8d6259b9
github-action-benchmark <github@users.noreply.github.com>
2023-01-28 04:27:56 +0000

    add Aleo SDK Benchmarks (cargo) benchmark result for 948fc777782b959a402cb956ec2ada089e8a93ac

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1674880076054,
+  "repoUrl": "https://github.com/AleoHQ/aleo",
+  "entries": {
+    "Aleo SDK Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "collin@aleo.org",
+            "name": "Collin Chin",
+            "username": "collinc97"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "948fc777782b959a402cb956ec2ada089e8a93ac",
+          "message": "Feat/typescript sdk (#453)",
+          "timestamp": "2023-01-27T20:17:34-08:00",
+          "tree_id": "9aa821bea16f564dbe4429ad90ba4100216359d2",
+          "url": "https://github.com/AleoHQ/aleo/commit/948fc777782b959a402cb956ec2ada089e8a93ac"
+        },
+        "date": 1674880072750,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "testnet3_address_from_private_key",
+            "value": 246477,
+            "range": "± 43463",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "testnet3_private_key_new",
+            "value": 98091,
+            "range": "± 13727",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
