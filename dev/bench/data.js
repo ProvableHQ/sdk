acc59958a6dee73af59473c45c3347d36ed6ac1e
github-action-benchmark <github@users.noreply.github.com>
2023-01-30 23:10:38 +0000

    add Aleo SDK Benchmarks (cargo) benchmark result for 95277d5abc83b959e074c37fa0e12590cca65309

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1675120238333,
+  "repoUrl": "https://github.com/AleoHQ/aleo",
+  "entries": {
+    "Aleo SDK Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "16715212+collinc97@users.noreply.github.com",
+            "name": "collin",
+            "username": "collinc97"
+          },
+          "committer": {
+            "email": "16715212+collinc97@users.noreply.github.com",
+            "name": "collin",
+            "username": "collinc97"
+          },
+          "distinct": true,
+          "id": "95277d5abc83b959e074c37fa0e12590cca65309",
+          "message": "fix website",
+          "timestamp": "2023-01-30T15:06:25-08:00",
+          "tree_id": "dac2e2beb12a89d57b34bb2d998b9a87cb78894c",
+          "url": "https://github.com/AleoHQ/aleo/commit/95277d5abc83b959e074c37fa0e12590cca65309"
+        },
+        "date": 1675120234796,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "testnet3_address_from_private_key",
+            "value": 246859,
+            "range": "± 17906",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "testnet3_private_key_new",
+            "value": 99060,
+            "range": "± 3199",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
