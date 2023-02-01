afeaad392e93b84f081ae16782d1d3e0c0f5ebf5
github-action-benchmark <github@users.noreply.github.com>
2023-02-01 18:47:20 +0000

    add Aleo SDK Benchmarks (cargo) benchmark result for 3147353935e8de2d9313ae4ad9834571053057dd

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1675277240759,
+  "repoUrl": "https://github.com/AleoHQ/aleo",
+  "entries": {
+    "Aleo SDK Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "ljedrz@users.noreply.github.com",
+            "name": "ljedrz",
+            "username": "ljedrz"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3147353935e8de2d9313ae4ad9834571053057dd",
+          "message": "chore: update the license headers (#457)\n\nSigned-off-by: ljedrz <ljedrz@gmail.com>",
+          "timestamp": "2023-02-01T10:40:09-08:00",
+          "tree_id": "406268de1c808a77a21633bc9221303439d1be1c",
+          "url": "https://github.com/AleoHQ/aleo/commit/3147353935e8de2d9313ae4ad9834571053057dd"
+        },
+        "date": 1675277238042,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "testnet3_address_from_private_key",
+            "value": 192450,
+            "range": "± 25727",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "testnet3_private_key_new",
+            "value": 77013,
+            "range": "± 12995",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
