62287d5ebb27e138cf5fbc25186aa85cfd9fda5f
github-action-benchmark <github@users.noreply.github.com>
2023-02-02 18:06:46 +0000

    add Aleo SDK Benchmarks (cargo) benchmark result for 216aa7ec2ebb42bf45a167683be4c1f5819e7d1b

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1675361206292,
+  "repoUrl": "https://github.com/AleoHQ/aleo",
+  "entries": {
+    "Aleo SDK Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "collin@aleo.org",
+            "name": "Collin Chin",
+            "username": "collinc97"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "216aa7ec2ebb42bf45a167683be4c1f5819e7d1b",
+          "message": "fix ci typo for m1 release (#458)\n\n* fix ci typo for m1 release\r\n\r\n* make aleo update less verbose",
+          "timestamp": "2023-02-02T09:59:20-08:00",
+          "tree_id": "36fb99f038127e4c4ae2e37d60b5ba710a5eafc9",
+          "url": "https://github.com/AleoHQ/aleo/commit/216aa7ec2ebb42bf45a167683be4c1f5819e7d1b"
+        },
+        "date": 1675361203152,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "testnet3_address_from_private_key",
+            "value": 240112,
+            "range": "± 21155",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "testnet3_private_key_new",
+            "value": 94981,
+            "range": "± 444",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
