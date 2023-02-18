d154162f11478b4ae301e84c8b3e3d265654f47a
Collin Chin <collin@aleo.org>
2023-02-17 20:42:47 -0500

    Create integation test script for Aleo CLI (#476)
    * commit integation test script
    
    * add aleo integration test to circlci
    
    * ALEO circleci command
    
    * run script with bash
    
    * rename circleci caches
    
    * give circle ci permission to execute aleo-new test
    
    * give permission locally
    
    * Remove dangling import
    
    * Remove js dangling imports
    
    * Test if reverting circleci image fixes illegal instruction issue
    
    * Revert to 1.67
    
    * Remove commited program files
    
    * Test if not transferring cache removes the illegal instruction
    
    * Test if not transferring cache removes the illegal instruction
    
    * Script location fix
    
    * 2nd Script location fix
    
    * Ci script fix
    
    ---------
    
    Co-authored-by: Michael Turner <mturner@aleo.org>

---
+++ .circleci/aleo-new.sh
@@ -0,0 +1,24 @@
+echo "Downloading parameters. This may take a few minutes..."
+
+# Create a new foo Leo project.
+$ALEO new foo || exit
+cd foo || exit
+
+# Attempt to compile the foo program until it passes.
+# This is necessary to ensure that the universal parameters are downloaded.
+declare -i DONE
+
+DONE=1
+
+while [ $DONE -ne 0 ]
+do
+$ALEO build
+DONE=$?
+sleep 0.5
+done
+
+# Try to run `aleo run`.
+$ALEO run hello 1u32 2u32 || exit
+
+# Remove the foo program.
+cd .. && rm -rf foo
