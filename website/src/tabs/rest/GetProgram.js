9f0c5f1243d60b0e1917ce0ed8417c806156e863
collin <16715212+collinc97@users.noreply.github.com>
2023-02-27 17:39:16 -0800

    use search bar for get program id

--- website/src/tabs/rest/GetProgram.js
+++ website/src/tabs/rest/GetProgram.js
@@ -5,35 +5,67 @@ import { CopyButton } from "../../components/CopyButton";
 
 export const GetProgram = () => {
     const [program, setProgram] = useState(null);
-    const [programName, setProgramName] = useState(null);
+    const [programID, setProgramID] = useState(null);
+    const [programExists, setProgramExists] = useState(false);
 
+    // Returns the program id if the user changes it or the "Demo" button is clicked.
     const onChange = (event) => {
+        if (event.target.value !== null) {
+            setProgramID(event.target.value);
+        }
+        return programID;
+    }
+
+    // Calls `tryRequest` when the search bar is triggered.
+    const onSearch = (value) => {
         try {
-            tryRequest(event.target.value);
+            tryRequest(value);
         } catch (error) {
             console.error(error);
         }
     };
 
+    // Attempts to request the program bytecode with the given program id.
     const tryRequest = (id) => {
-        setProgramName(id);
+        setProgramID(id);
         try {
             if (id) {
                 axios
                     .get(`https://vm.aleo.org/api/testnet3/program/${id}`)
-                    .then((response) =>
-                        setProgram(response.data)
-                    );
+                    .then((response) => {
+                        setProgramExists(true);
+                        setProgram(response.data);
+                    })
+                    .catch((error) => {
+                        // Reset the program text to `null` if the program id does not exist.
+                        setProgram(null);
+                        console.error(error);
+                    });
+
+            } else {
+                // Reset the program text if the user clears the search bar.
+                setProgram(null);
             }
         } catch (error) {
             console.error(error);
         }
     };
 
+    // Returns the `validateStatus` string to set the highlighting around the search bar.
+    // ""        = grey
+    // "success" = green
+    // "error"   = red
+    const validateStatusProgram = () => {
+        return programExists ?
+            program !== null ?
+                "success"
+                : "error"
+            : "";
+    }
 
     const layout = { labelCol: { span: 3 }, wrapperCol: { span: 21 } };
     const programString = () => program !== null ? program : "";
-    const programNameString = () => programName !== null ? programName : "";
+    const programIDString = () => programID !== null ? programID : "";
 
     return <Card title="Get Program"
                  style={{width: "100%", borderRadius: "20px"}}
@@ -41,9 +73,18 @@ export const GetProgram = () => {
                  extra={<Button type="primary" shape="round" size="middle" onClick={() => {tryRequest("credits.aleo")}}
     >Demo</Button>}>
         <Form {...layout}>
-            <Form.Item label="Program Name" colon={false}>
-                <Input name="id" size="large" placeholder="Program Name" allowClear onChange={onChange}
-                       value={programNameString()} style={{borderRadius: '20px'}}/>
+            <Form.Item label="Program ID"
+                       colon={false}
+                       validateStatus={validateStatusProgram()}
+            >
+                <Input.Search name="id"
+                              size="large"
+                              placeholder="Program ID"
+                              allowClear
+                              onSearch={onSearch}
+                              onChange={onChange}
+                              value={programIDString()}
+                              style={{borderRadius: '20px'}}/>
             </Form.Item>
         </Form>
         {
@@ -52,7 +93,7 @@ export const GetProgram = () => {
                     <Divider/>
                     <Row align="middle">
                         <Col span={23}>
-                            <Form.Item label="Program" colon={false}>
+                            <Form.Item label="Program Bytecode" colon={false}>
                                 <Input.TextArea size="large" rows={15} placeholder="Program" style={{whiteSpace: 'pre-wrap', overflowWrap: 'break-word'}}
                                                 value={programString()}
                                                 disabled/>
