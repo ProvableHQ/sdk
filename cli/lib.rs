c367042f4a7351313dad6e4edc4bcc56d09dc5ca
Michael Turner <mturner@aleo.org>
2023-03-14 09:52:47 -0400

    Disable the Rust CLI when using the wasm feature flag

--- cli/lib.rs
+++ cli/lib.rs
@@ -16,12 +16,18 @@
 
 #![forbid(unsafe_code)]
 
+#[cfg(not(feature = "wasm"))]
 #[macro_use]
 extern crate thiserror;
 
+#[cfg(not(feature = "wasm"))]
 pub mod commands;
+#[cfg(not(feature = "wasm"))]
 pub mod errors;
+#[cfg(not(feature = "wasm"))]
 pub mod helpers;
 
+#[cfg(not(feature = "wasm"))]
 pub type CurrentNetwork = snarkvm::prelude::Testnet3;
+#[cfg(not(feature = "wasm"))]
 pub type Aleo = snarkvm::circuit::AleoV0;
