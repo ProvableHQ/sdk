7b1eb8f970f412d59732ffd1fa66cebba6b761d5
github-action-benchmark <github@users.noreply.github.com>
2023-03-28 05:41:12 +0000

    add Aleo SDK Benchmarks (cargo) benchmark result for e09b9398146cc1b2c9af8fd476ed660af49dc174

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1679981967845,
+  "lastUpdate": 1679982072349,
   "repoUrl": "https://github.com/AleoHQ/aleo",
   "entries": {
     "Aleo SDK Benchmarks": [
@@ -53,6 +53,60 @@ window.BENCHMARK_DATA = {
             "name": "testnet3_private_key_encryption_decryption_roundtrip",
             "value": 600973,
             "range": "± 1780",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e09b9398146cc1b2c9af8fd476ed660af49dc174",
+          "message": "build(deps): bump walkdir from 2.3.2 to 2.3.3 (#505)\n\nBumps [walkdir](https://github.com/BurntSushi/walkdir) from 2.3.2 to 2.3.3.\r\n- [Release notes](https://github.com/BurntSushi/walkdir/releases)\r\n- [Commits](https://github.com/BurntSushi/walkdir/compare/2.3.2...2.3.3)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: walkdir\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-03-27T22:20:57-07:00",
+          "tree_id": "d51f29a481e9fc33480b0760cb6efcf5829474bb",
+          "url": "https://github.com/AleoHQ/aleo/commit/e09b9398146cc1b2c9af8fd476ed660af49dc174"
+        },
+        "date": 1679982069256,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "testnet3_address_from_private_key",
+            "value": 252641,
+            "range": "± 1782",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "testnet3_private_key_new",
+            "value": 101541,
+            "range": "± 776",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "testnet3_private_key_decryption",
+            "value": 368950,
+            "range": "± 3218",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "testnet3_private_key_encryption",
+            "value": 262138,
+            "range": "± 3122",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "testnet3_private_key_encryption_decryption_roundtrip",
+            "value": 639280,
+            "range": "± 3329",
             "unit": "ns/iter"
           }
         ]
