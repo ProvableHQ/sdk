0a39cb345d96de5af7c7a9efd7fefa8c33f1abe8
github-action-benchmark <github@users.noreply.github.com>
2023-05-03 23:24:22 +0000

    add Aleo SDK Benchmarks (cargo) benchmark result for 9699604042be33a3a56d6bfa9dcab7f2f0f77477

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1683156192411,
+  "lastUpdate": 1683156262911,
   "repoUrl": "https://github.com/AleoHQ/aleo",
   "entries": {
     "Aleo SDK Benchmarks": [
@@ -53,6 +53,60 @@ window.BENCHMARK_DATA = {
             "name": "testnet3_private_key_encryption_decryption_roundtrip",
             "value": 597962,
             "range": "± 1970",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9699604042be33a3a56d6bfa9dcab7f2f0f77477",
+          "message": "build(deps): bump anyhow from 1.0.70 to 1.0.71 (#554)\n\nBumps [anyhow](https://github.com/dtolnay/anyhow) from 1.0.70 to 1.0.71.\r\n- [Release notes](https://github.com/dtolnay/anyhow/releases)\r\n- [Commits](https://github.com/dtolnay/anyhow/compare/1.0.70...1.0.71)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: anyhow\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-05-03T16:03:55-07:00",
+          "tree_id": "7b93ab5ca7987ae71ca2bb562f302145eb70a166",
+          "url": "https://github.com/AleoHQ/aleo/commit/9699604042be33a3a56d6bfa9dcab7f2f0f77477"
+        },
+        "date": 1683156258541,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "testnet3_address_from_private_key",
+            "value": 254047,
+            "range": "± 33892",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "testnet3_private_key_new",
+            "value": 100829,
+            "range": "± 1683",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "testnet3_private_key_decryption",
+            "value": 365105,
+            "range": "± 14889",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "testnet3_private_key_encryption",
+            "value": 263114,
+            "range": "± 982",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "testnet3_private_key_encryption_decryption_roundtrip",
+            "value": 641462,
+            "range": "± 2387",
             "unit": "ns/iter"
           }
         ]
