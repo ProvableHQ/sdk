08a23e4a716a55b8f0bb892b182266a20c53dfe9
Michael Turner <mturner@aleo.org>
2023-05-23 16:49:51 -0400

    Remove log statements

--- website/src/workers/worker.js
+++ website/src/workers/worker.js
@@ -2,12 +2,11 @@ import init, * as aleo from '@aleohq/wasm';
 
 await init();
 
-await aleo.initThreadPool(10);
+aleo.initThreadPool(8);
 
 const aleoProgramManager = aleo.ProgramManager.new();
 self.addEventListener("message", ev => {
     if (ev.data.type === 'ALEO_EXECUTE_PROGRAM_LOCAL') {
-        console.log("Web worker: Received message to execute program locally");
         const {
             localProgram,
             aleoFunction,
@@ -15,28 +14,20 @@ self.addEventListener("message", ev => {
             privateKey,
         } = ev.data;
 
-        console.log('Web worker: Executing Local Function...');
+        console.log('Web worker: Executing function locally...');
         let startTime = performance.now();
-        console.log(aleo);
-        console.log("Aleo program manager", aleoProgramManager);
-        let key = aleo.PrivateKey.from_string(privateKey);
-        console.log(key);
-        console.log(localProgram);
-        console.log(inputs);
-        console.log(privateKey);
-        console.log("return to string", key.to_string());
-        console.log("ciphertext", key.toCiphertext("ass").toString());
+
         let response = aleoProgramManager.execute_local(
             localProgram,
             aleoFunction,
             inputs,
-            key,
+            aleo.PrivateKey.from_string(privateKey),
             true
         );
 
-        console.log(`Web worker: Execution Completed: ${performance.now() - startTime} ms`);
+        console.log(`Web worker: Local execution completed in ${performance.now() - startTime} ms`);
         let outputs = response.getOutputs();
-        console.log(`Function Execution Response: ${outputs}`);
+        console.log(`Function execution response: ${outputs}`);
         self.postMessage({ type: 'OFFLINE_EXECUTION_COMPLETED',  outputs });
     }
     else if (ev.data.type === 'ALEO_EXECUTE_PROGRAM_ON_CHAIN') {
@@ -51,14 +42,8 @@ self.addEventListener("message", ev => {
         } = ev.data;
 
         console.log('Web worker: Creating execution...');
-
         let startTime = performance.now();
-        console.log(remoteProgram);
-        console.log(aleoFunction);
-        console.log(inputs);
-        console.log("fee in gates: ", fee*1000000);
-        console.log(feeRecord);
-        console.log(url);
+
         (async function() {
             let executeTransaction = await aleoProgramManager.execute(
                 remoteProgram,
@@ -70,7 +55,7 @@ self.addEventListener("message", ev => {
                 url
             );
 
-            console.log(`Web worker: Execution Transaction Verified: ${performance.now() - startTime} ms`);
+            console.log(`Web worker: On-chain execution transaction created in ${performance.now() - startTime} ms`);
             console.log(executeTransaction.toString());
             self.postMessage({ type: 'EXECUTION_TRANSACTION_COMPLETED', executeTransaction: executeTransaction.toString() });
         })();
@@ -87,8 +72,8 @@ self.addEventListener("message", ev => {
         } = ev.data;
 
         console.log('Web worker: Creating transfer...');
-
         let startTime = performance.now();
+
         (async function() {
             let transferTransaction = await aleoProgramManager.transfer(
                 aleo.PrivateKey.from_string(privateKey),
@@ -100,7 +85,7 @@ self.addEventListener("message", ev => {
                 url
             );
 
-            console.log(`Web worker: Transfer Transaction Verified: ${performance.now() - startTime} ms`);
+            console.log(`Web worker: Transfer transaction created in ${performance.now() - startTime} ms`);
             console.log(transferTransaction.toString());
             self.postMessage({ type: 'TRANSFER_TRANSACTION_COMPLETED', transferTransaction: transferTransaction.toString() });
         })();
@@ -127,7 +112,7 @@ self.addEventListener("message", ev => {
                 url,
             );
 
-            console.log(`Web worker: Deployment Transaction Verified: ${performance.now() - startTime} ms`);
+            console.log(`Web worker: Deployment transaction created in ${performance.now() - startTime} ms`);
             console.log(deployTransaction.toString());
             self.postMessage({ type: 'DEPLOY_TRANSACTION_COMPLETED', deployTransaction: deployTransaction.toString() });
         })();
