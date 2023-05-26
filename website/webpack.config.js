0653a2313379517369e88ccf67ab3961f8c0005d
iamalwaysuncomfortable <zaphrod.beeblebrox@gmail.com>
2023-05-26 18:23:03 -0400

    Revert website changes

--- website/webpack.config.js
+++ website/webpack.config.js
@@ -2,24 +2,14 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require("copy-webpack-plugin");
 
-const appConfig = {
+module.exports = {
     mode: 'production',
-    entry: {
-        index: './src/index.js'
-    },
     output: {
         path: path.join(__dirname, '/dist'),
-        filename: '[name].bundle.js'
+        filename: 'index.bundle.js'
     },
     devServer: {
         port: 3000,
-        headers: {
-            'Cross-Origin-Opener-Policy': 'same-origin',
-            'Cross-Origin-Embedder-Policy': 'require-corp'
-        },
-        client: {
-            overlay: false
-        }
     },
     module: {
         rules: [
@@ -43,41 +33,15 @@ const appConfig = {
             ]
         }),
         new HtmlWebpackPlugin(
-            {
-                template: './public/index.html'
-            }),
-    ],
+        {
+            template: './public/index.html'
+        })],
     performance: {
-        hints: false,
-        maxAssetSize: 13 * 1024 * 1024, // 12 MiB
-        maxEntrypointSize: 13 * 1024 * 1024, // 12 MiB
-    },
-    stats: {
-        warnings: false,
+        maxEntrypointSize: 8388608,
+        maxAssetSize: 8388608
     },
     experiments: {
-        asyncWebAssembly: true,
-        topLevelAwait: true
+        asyncWebAssembly: true
     },
-    devtool: 'source-map',
+    devtool: false,
 }
-
-const workerConfig = {
-    mode: 'production',
-    entry: "./src/workers/worker.js",
-    target: "webworker",
-    resolve: {
-        extensions: [".js", ".wasm"]
-    },
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: "worker.js"
-    },
-    experiments: {
-        asyncWebAssembly: true,
-        topLevelAwait: true
-    },
-    devtool: 'source-map',
-};
-
-module.exports = [appConfig, workerConfig];
