4eede44909a988013e5b1695f92f79bddc6b9b9a
Michael Turner <mturner@aleo.org>
2023-06-19 18:31:47 -0400

    Add check to ensure records provided have enough funds to execute a transfer

--- rust/src/program/transfer.rs
+++ rust/src/program/transfer.rs
@@ -25,11 +25,15 @@ impl<N: Network> ProgramManager<N> {
         fee: u64,
         recipient_address: Address<N>,
         password: Option<&str>,
-        input_record: Record<N, Plaintext<N>>,
+        amount_record: Record<N, Plaintext<N>>,
         fee_record: Record<N, Plaintext<N>>,
     ) -> Result<String> {
-        ensure!(amount > 0, "Amount must be greater than 0");
-        ensure!(fee > 0, "Fee must be greater than 0");
+        // Ensure records provided have enough credits to cover the transfer amount and fee
+        ensure!(
+            amount_record.microcredits()? >= amount,
+            "Credits in amount record must greater than transfer amount specified"
+        );
+        ensure!(fee_record.microcredits()? >= fee, "Fee must be greater than 0");
 
         // Specify the network state query
         let query = Query::from(self.api_client.as_ref().unwrap().base_url());
@@ -47,7 +51,7 @@ impl<N: Network> ProgramManager<N> {
 
             // Prepare the inputs for a transfer.
             let inputs = vec![
-                Value::Record(input_record),
+                Value::Record(amount_record),
                 Value::from_str(&recipient_address.to_string())?,
                 Value::from_str(&format!("{}u64", amount))?,
             ];
