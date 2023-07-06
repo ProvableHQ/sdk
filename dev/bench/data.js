4ec7228c566e3ab08db4b1d998249535d11d8100
github-action-benchmark <github@users.noreply.github.com>
2023-07-06 11:01:17 +0000

    add Aleo SDK Benchmarks (cargo) benchmark result for b9f90a6cd63022150f21b32a4e52ad6d247900fa

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1688641088143,
+  "lastUpdate": 1688641277767,
   "repoUrl": "https://github.com/AleoHQ/sdk",
   "entries": {
     "Aleo SDK Benchmarks": [
@@ -53,6 +53,60 @@ window.BENCHMARK_DATA = {
             "name": "testnet3_private_key_encryption_decryption_roundtrip",
             "value": 607922,
             "range": "± 3285",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mturner@aleo.org",
+            "name": "Mike Turner",
+            "username": "iamalwaysuncomfortable"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b9f90a6cd63022150f21b32a4e52ad6d247900fa",
+          "message": "[chore] build(deps): bump colored from 2.0.3 to 2.0.4",
+          "timestamp": "2023-07-06T06:40:40-04:00",
+          "tree_id": "f97fb2d536160d414fe72f24e8a4c8fc3e48ecb8",
+          "url": "https://github.com/AleoHQ/sdk/commit/b9f90a6cd63022150f21b32a4e52ad6d247900fa"
+        },
+        "date": 1688641265626,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "testnet3_address_from_private_key",
+            "value": 266876,
+            "range": "± 48984",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "testnet3_private_key_new",
+            "value": 109359,
+            "range": "± 11311",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "testnet3_private_key_decryption",
+            "value": 420204,
+            "range": "± 49280",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "testnet3_private_key_encryption",
+            "value": 297981,
+            "range": "± 48163",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "testnet3_private_key_encryption_decryption_roundtrip",
+            "value": 716382,
+            "range": "± 66963",
             "unit": "ns/iter"
           }
         ]
