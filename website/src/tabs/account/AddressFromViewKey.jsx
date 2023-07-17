5d5fa754f08beba6b2f15178f9a0c0dc04f2d8bc
Brent Conn <brent.conn@gmail.com>
2023-07-17 12:02:35 -0400

    pretty new files

--- website/src/tabs/account/AddressFromViewKey.jsx
+++ website/src/tabs/account/AddressFromViewKey.jsx
@@ -1,7 +1,7 @@
-import React, {useState} from "react";
-import {Card, Divider, Form, Input} from "antd";
-import {CopyButton} from "../../components/CopyButton";
-import {useAleoWASM} from "../../aleo-wasm-hook";
+import { useState } from "react";
+import { Card, Divider, Form, Input } from "antd";
+import { CopyButton } from "../../components/CopyButton";
+import { useAleoWASM } from "../../aleo-wasm-hook";
 
 export const AddressFromViewKey = () => {
     const [addressFromViewKey, setAddressFromViewKey] = useState(null);
@@ -10,40 +10,64 @@ export const AddressFromViewKey = () => {
     const onChange = (event) => {
         setAddressFromViewKey(null);
         try {
-            setAddressFromViewKey(aleo.ViewKey.from_string(event.target.value))
+            setAddressFromViewKey(aleo.ViewKey.from_string(event.target.value));
         } catch (error) {
             console.error(error);
         }
-    }
+    };
 
-    const layout = {labelCol: {span: 3}, wrapperCol: {span: 21}};
+    const layout = { labelCol: { span: 3 }, wrapperCol: { span: 21 } };
 
     if (aleo !== null) {
-        const address = () => addressFromViewKey !== null ? addressFromViewKey.to_address().to_string() : "";
+        const address = () =>
+            addressFromViewKey !== null
+                ? addressFromViewKey.to_address().to_string()
+                : "";
 
-        return <Card title="Load Address from View Key" style={{width: "100%", borderRadius: "20px"}}
-                     bordered={false}>
-            <Form {...layout}>
-                <Form.Item label="View Key" colon={false}>
-                    <Input name="viewKey" size="large" placeholder="View Key" allowClear onChange={onChange}
-                           style={{borderRadius: '20px'}}/>
-                </Form.Item>
-            </Form>
-            {
-                (addressFromViewKey !== null) ?
+        return (
+            <Card
+                title="Load Address from View Key"
+                style={{ width: "100%", borderRadius: "20px" }}
+                bordered={false}
+            >
+                <Form {...layout}>
+                    <Form.Item label="View Key" colon={false}>
+                        <Input
+                            name="viewKey"
+                            size="large"
+                            placeholder="View Key"
+                            allowClear
+                            onChange={onChange}
+                            style={{ borderRadius: "20px" }}
+                        />
+                    </Form.Item>
+                </Form>
+                {addressFromViewKey !== null ? (
                     <Form {...layout}>
-                        <Divider/>
+                        <Divider />
                         <Form.Item label="Address" colon={false}>
-                            <Input size="large" placeholder="Address" value={address()}
-                                   addonAfter={<CopyButton data={address()} style={{borderRadius: '20px'}}/>} disabled/>
+                            <Input
+                                size="large"
+                                placeholder="Address"
+                                value={address()}
+                                addonAfter={
+                                    <CopyButton
+                                        data={address()}
+                                        style={{ borderRadius: "20px" }}
+                                    />
+                                }
+                                disabled
+                            />
                         </Form.Item>
                     </Form>
-                    : null
-            }
-        </Card>
+                ) : null}
+            </Card>
+        );
     } else {
-        return <h3>
-            <center>Loading...</center>
-        </h3>
+        return (
+            <h3>
+                <center>Loading...</center>
+            </h3>
+        );
     }
-}
+};
