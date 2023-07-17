727ab2732b9ee205dd594a9077d64d95a087af4a
Brent C <brent@aleo.org>
2023-07-17 12:40:28 -0400

    [Chore] aleo.tools prettier and eslint fixes (#664)
    * prettier installation and run
    
    * eslint fixes
    
    * adding lint to ci checks
    
    * fixing new prettier issues, removing cicd check until we can address errors
    
    * pretty new files

--- website/src/tabs/rest/GetLatestBlockHeight.jsx
+++ website/src/tabs/rest/GetLatestBlockHeight.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import {Button, Card, Col, Divider, Form, Input, Row} from "antd";
+import { useState } from "react";
+import { Button, Card, Col, Divider, Form, Input, Row } from "antd";
 import axios from "axios";
 import { CopyButton } from "../../components/CopyButton";
 
@@ -12,34 +12,56 @@ export const GetLatestBlockHeight = () => {
             axios
                 .get(`https://vm.aleo.org/api/testnet3/latest/height`)
                 .then((response) =>
-                    setLatestHeight(JSON.stringify(response.data, null, 2))
+                    setLatestHeight(JSON.stringify(response.data, null, 2)),
                 );
         } catch (error) {
             console.error(error);
         }
     };
 
-    const layout = {labelCol: {span: 3}, wrapperCol: {span: 21}};
+    const layout = { labelCol: { span: 3 }, wrapperCol: { span: 21 } };
 
     const latestHeightString = () =>
         latestHeight !== null ? latestHeight.toString() : "";
 
-    return <Card title="Get Latest Block Height" style={{width: "100%", borderRadius: "20px"}} bordered={false}>
-        <Row justify="center">
-            <Col><Button type="primary" shape="round" size="middle" onClick={tryRequest}
-                         >Get Latest Block Height</Button></Col>
-        </Row>
-        {
-            (latestHeight !== null) ?
+    return (
+        <Card
+            title="Get Latest Block Height"
+            style={{ width: "100%", borderRadius: "20px" }}
+            bordered={false}
+        >
+            <Row justify="center">
+                <Col>
+                    <Button
+                        type="primary"
+                        shape="round"
+                        size="middle"
+                        onClick={tryRequest}
+                    >
+                        Get Latest Block Height
+                    </Button>
+                </Col>
+            </Row>
+            {latestHeight !== null ? (
                 <Form {...layout}>
-                    <Divider/>
+                    <Divider />
                     <Form.Item label="Block" colon={false}>
-                        <Input size="large" rows={1} placeholder="Block" value={latestHeightString()}
-                                        addonAfter={<CopyButton data={latestHeightString()} style={{borderRadius: '20px'}}/>}
-                                        disabled/>
+                        <Input
+                            size="large"
+                            rows={1}
+                            placeholder="Block"
+                            value={latestHeightString()}
+                            addonAfter={
+                                <CopyButton
+                                    data={latestHeightString()}
+                                    style={{ borderRadius: "20px" }}
+                                />
+                            }
+                            disabled
+                        />
                     </Form.Item>
                 </Form>
-                : null
-        }
-    </Card>
-}
+            ) : null}
+        </Card>
+    );
+};
