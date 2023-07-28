ed3a2eb4fc8d470b1fea5f918c7fbd37a2b51b36
Brent Conn <brent.conn@gmail.com>
2023-07-28 10:34:30 -0400

    execute offline working

--- website/src/tabs/develop/execute/index.jsx
+++ website/src/tabs/develop/execute/index.jsx
@@ -13,25 +13,108 @@ import {
 import { LoadProgram } from "./LoadProgram.jsx";
 import { CodeEditor } from "./CodeEditor.jsx";
 import { useAleoWASM } from "../../../aleo-wasm-hook";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const layout = { labelCol: { span: 4 }, wrapperCol: { span: 18 } };
 
 export const Execute = () => {
     const [form] = Form.useForm();
-
     const aleoWASM = useAleoWASM();
-
-    const onFinish = (values) => {
-        console.log("Success:", values);
-    };
-    const onFinishFailed = (errorInfo) => {
-        console.log("Failed:", errorInfo);
-    };
 
     const demoSelect = (value) => {
         console.log(`selected ${value}`);
     };
+
+    const [worker, setWorker] = useState(null);
+
+    useEffect(() => {
+        if (worker === null) {
+            const spawnedWorker = spawnWorker();
+            setWorker(spawnedWorker);
+            return () => {
+                spawnedWorker.terminate();
+            };
+        }
+    }, []);
+
+    const execute = async (values) => {
+        const { program, functionName, inputs, private_key } = values;
+        await postMessagePromise(worker, {
+            type: "ALEO_EXECUTE_PROGRAM_LOCAL",
+            localProgram: program,
+            aleoFunction: functionName,
+            inputs: JSON.parse(inputs),
+            privateKey: private_key,
+        });
+    };
+
+    function postMessagePromise(worker, message) {
+        return new Promise((resolve, reject) => {
+            worker.onmessage = (event) => {
+                resolve(event.data);
+            };
+            worker.onerror = (error) => {
+                reject(error);
+            };
+            worker.postMessage(message);
+        });
+    }
+
+    function spawnWorker() {
+        let worker = new Worker(
+            new URL("../../../workers/worker.js", import.meta.url),
+            { type: "module" },
+        );
+        worker.addEventListener("message", (ev) => {
+            if (ev.data.type == "OFFLINE_EXECUTION_COMPLETED") {
+                // setFeeLoading(false);
+                // setLoading(false);
+                // setTransactionID(null);
+                // setExecutionError(null);
+                // setProgramResponse(ev.data.outputs);
+                // setTip("Executing Program...");
+                console.log("Offline execution complete " + ev.data.outputs);
+            } else if (ev.data.type == "EXECUTION_TRANSACTION_COMPLETED") {
+                let [transaction, url] = ev.data.executeTransaction;
+                axios
+                    .post(
+                        url + "/testnet3/transaction/broadcast",
+                        transaction,
+                        {
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                        },
+                    )
+                    .then((response) => {
+                        // setFeeLoading(false);
+                        // setLoading(false);
+                        // setProgramResponse(null);
+                        // setExecutionError(null);
+                        // setTip("Executing Program...");
+                        // setTransactionID(response.data);
+                    });
+            } else if (ev.data.type == "EXECUTION_FEE_ESTIMATION_COMPLETED") {
+                // let fee = ev.data.executionFee;
+                // setFeeLoading(false);
+                // setLoading(false);
+                // setProgramResponse(null);
+                // setExecutionError(null);
+                // setTransactionID(null);
+                // setTip("Executing Program...");
+                // setExecutionFee(fee.toString());
+            } else if (ev.data.type == "ERROR") {
+                // setFeeLoading(false);
+                // setLoading(false);
+                // setProgramResponse(null);
+                // setTransactionID(null);
+                // setTip("Executing Program...");
+                // setExecutionError(ev.data.errorMessage);
+            }
+        });
+        return worker;
+    }
 
     const [program, setProgram] = useState(null);
     const [functions, setFunctions] = useState([]);
@@ -61,8 +144,6 @@ export const Execute = () => {
         const functionNames = processedProgram.getFunctions();
         const functionItems = functionNames.map((func, index) => {
             const functionInputs = processedProgram.getFunctionInputs(func);
-
-            console.log(functionInputs);
             return {
                 key: index,
                 label: func,
@@ -92,78 +173,138 @@ export const Execute = () => {
                 />
             }
         >
-            <Form
-                form={form}
-                name="execute"
-                {...layout}
-                onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
-                autoComplete="off"
-            >
-                <LoadProgram onResponse={onLoadProgram} />
-                <Form.Item
-                    label="Program"
-                    name="program"
-                    rules={[
-                        {
-                            required: true,
-                            message: "Please input or load an Aleo program",
-                        },
-                    ]}
-                >
-                    <CodeEditor onChange={onProgramEdit} />
-                </Form.Item>
-                <Divider dashed />
-                <Form.Item
-                    label="Private Key"
-                    name="private_key"
-                    rules={[
-                        {
-                            required: true,
-                            message: "Private key required to execute program",
-                        },
-                    ]}
-                >
-                    <Input />
-                </Form.Item>
-                <Divider dashed />
-                <Form.Item
-                    label="Execute On-Chain"
-                    name="execute_onchain"
-                    valuePropName="checked"
-                >
-                    <Switch />
-                </Form.Item>
-                <Form.Item
-                    noStyle
-                    shouldUpdate={(prevValues, currentValues) =>
-                        prevValues.execute_onchain !==
-                        currentValues.execute_onchain
+            <Form.Provider
+                onFormFinish={(name, info) => {
+                    if (name !== "execute") {
+                        form.setFieldValue("functionName", name);
+                        form.setFieldValue(
+                            "inputs",
+                            JSON.stringify(info.values.inputs),
+                        );
+                        form.submit();
                     }
+                }}
+            >
+                <Form
+                    form={form}
+                    name="execute"
+                    {...layout}
+                    onFinish={execute}
+                    autoComplete="off"
+                    scrollToFirstError="true"
                 >
-                    {({ getFieldValue }) =>
-                        getFieldValue("execute_onchain") === true ? (
+                    <LoadProgram onResponse={onLoadProgram} />
+                    <Form.Item
+                        label="Program"
+                        name="program"
+                        rules={[
+                            {
+                                required: true,
+                                message: "Please input or load an Aleo program",
+                            },
+                        ]}
+                    >
+                        <CodeEditor onChange={onProgramEdit} />
+                    </Form.Item>
+                    <Divider dashed />
+                    <Form.Item
+                        label="Private Key"
+                        name="private_key"
+                        rules={[
+                            {
+                                required: true,
+                                message:
+                                    "Private key required to execute program",
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Divider dashed />
+                    <Form.Item
+                        label="Execute On-Chain"
+                        name="execute_onchain"
+                        valuePropName="checked"
+                        initialValue={false}
+                    >
+                        <Switch />
+                    </Form.Item>
+                    <Form.Item
+                        noStyle
+                        shouldUpdate={(prevValues, currentValues) =>
+                            prevValues.execute_onchain !==
+                            currentValues.execute_onchain
+                        }
+                    >
+                        {({ getFieldValue }) => (
                             <>
-                                <Form.Item label="Peer URL" name="peer_url">
+                                <Form.Item
+                                    label="Peer URL"
+                                    name="peer_url"
+                                    hidden={!getFieldValue("execute_onchain")}
+                                >
                                     <Input />
                                 </Form.Item>
-                                <Form.Item label="Fee" name="fee">
+                                <Form.Item
+                                    label="Fee"
+                                    name="fee"
+                                    hidden={!getFieldValue("execute_onchain")}
+                                >
                                     <Input.Search enterButton="Estimate Fee" />
                                 </Form.Item>
-                                <Form.Item label="Fee Record" name="fee_record">
+                                <Form.Item
+                                    label="Fee Record"
+                                    name="fee_record"
+                                    hidden={!getFieldValue("execute_onchain")}
+                                >
                                     <Input.TextArea />
                                 </Form.Item>
                             </>
-                        ) : null
-                    }
-                </Form.Item>
-            </Form>
-            <Divider dashed>Program Functions</Divider>
-            {functions.length > 0 ? (
-                <Collapse bordered={false} items={functions} />
-            ) : (
-                <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
-            )}
+                        )}
+                    </Form.Item>
+                    <Divider dashed />
+                    <Form.Item
+                        label="Manual Input"
+                        name="manual_input"
+                        valuePropName="checked"
+                        initialValue={false}
+                    >
+                        <Switch />
+                    </Form.Item>
+                    <Form.Item
+                        noStyle
+                        shouldUpdate={(prevValues, currentValues) =>
+                            prevValues.manual_input !==
+                            currentValues.manual_input
+                        }
+                    >
+                        {({ getFieldValue }) => (
+                            <>
+                                <Form.Item
+                                    label="Function"
+                                    name="functionName"
+                                    hidden={!getFieldValue("manual_input")}
+                                >
+                                    <Input />
+                                </Form.Item>
+                                <Form.Item
+                                    label="Inputs"
+                                    name="inputs"
+                                    hidden={!getFieldValue("manual_input")}
+                                >
+                                    <Input.TextArea />
+                                </Form.Item>
+                            </>
+                        )}
+                    </Form.Item>
+                </Form>
+                <Divider dashed>Program Functions</Divider>
+                {functions.length > 0 ? (
+                    <Collapse bordered={false} items={functions} />
+                ) : (
+                    <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
+                )}
+            </Form.Provider>
         </Card>
     );
 };
@@ -180,24 +321,17 @@ const renderInput = (input, inputIndex, nameArray = []) => {
                     renderInput(
                         member,
                         memberIndex,
-                        []
-                            .concat(nameArray)
-                            .concat(input.name || input.register),
+                        [].concat(nameArray).concat(input.name || inputIndex),
                     ),
                 )}
             </div>
         );
     } else {
-        console.log(
-            ["inputs"].concat(nameArray).concat(input.name || input.register),
-        );
         return (
             <Form.Item
                 key={inputIndex}
                 label={input.name ? input.name : input.register}
-                name={["inputs"]
-                    .concat(nameArray)
-                    .concat(input.name || input.register)}
+                name={[].concat(nameArray).concat(input.name || inputIndex)}
                 rules={[{ required: true, message: "Please input a value" }]}
             >
                 <Input placeholder={`${input.type}`} />
@@ -206,19 +340,37 @@ const renderInput = (input, inputIndex, nameArray = []) => {
     }
 };
 
-const functionForm = (func, funcInputs) => (
-    <Form
-        name={func}
-        onFinish={(values) => console.log("Success:", values)}
-        onFinishFailed={() => console.log("on failed")}
-        autoComplete="off"
-        {...layout}
-    >
-        {funcInputs.length > 0 ? (
-            funcInputs.map((input, inputIndex) =>
-                renderInput(input, inputIndex, []),
-            )
-        ) : (
+const functionForm = (func, funcInputs) => {
+    return (
+        <Form
+            name={func}
+            autoComplete="off"
+            scrollToFirstError="true"
+            {...layout}
+        >
+            {funcInputs.length > 0 ? (
+                funcInputs.map((input, inputIndex) =>
+                    renderInput(input, inputIndex, ["inputs"]),
+                )
+            ) : (
+                <Form.Item
+                    wrapperCol={{
+                        xs: {
+                            offset: 0,
+                        },
+                        sm: {
+                            offset: 4,
+                            span: 18,
+                        },
+                    }}
+                >
+                    <Alert
+                        message={`The \`${func}\` function does not take any inputs.`}
+                        type="info"
+                        showIcon
+                    />
+                </Form.Item>
+            )}
             <Form.Item
                 wrapperCol={{
                     xs: {
@@ -226,30 +378,13 @@ const functionForm = (func, funcInputs) => (
                     },
                     sm: {
                         offset: 4,
-                        span: 18,
                     },
                 }}
             >
-                <Alert
-                    message={`The \`${func}\` function does not take any inputs.`}
-                    type="info"
-                    showIcon
-                />
+                <Button type="primary" htmlType="submit">
+                    Run
+                </Button>
             </Form.Item>
-        )}
-        <Form.Item
-            wrapperCol={{
-                xs: {
-                    offset: 0,
-                },
-                sm: {
-                    offset: 4,
-                },
-            }}
-        >
-            <Button type="primary" htmlType="submit">
-                Run
-            </Button>
-        </Form.Item>
-    </Form>
-);
+        </Form>
+    );
+};
