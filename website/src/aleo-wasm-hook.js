5ec71a79684cf7ac6538b94eb850a58032bdf4c4
Brent Conn <brent.conn@gmail.com>
2023-09-05 16:25:24 -0400

    using module-scoped vars to avoid redundant downloads

--- website/src/aleo-wasm-hook.js
+++ website/src/aleo-wasm-hook.js
@@ -1,18 +1,37 @@
 import { useEffect, useState } from "react";
 
+let loadingPromise = null;
+let loadedSDK = null;
+
 export const useAleoWASM = () => {
-    const [aleoInstance, setAleoInstance] = useState(null);
-    const [loading, setLoading] = useState(true);
+    const [aleoInstance, setAleoInstance] = useState(loadedSDK);
+    const [loading, setLoading] = useState(!loadedSDK);
 
     useEffect(() => {
-        import("@aleohq/sdk").then(async (sdk) => {
-            await sdk.initializeWasm();
-            setAleoInstance(sdk);
+        if (loadedSDK) {
+            setAleoInstance(loadedSDK);
             setLoading(false);
-        }).catch((error) => {
-            console.error("Failed to load the SDK:", error);
-            setLoading(false);
-        });
+            return;
+        }
+
+        if (!loadingPromise) {
+            loadingPromise = import("@aleohq/sdk")
+                .then(async (sdk) => {
+                    await sdk.initializeWasm();
+                    loadedSDK = sdk; // Save the loaded SDK
+                    setAleoInstance(sdk);
+                    setLoading(false);
+                })
+                .catch((error) => {
+                    console.error("Failed to load the SDK:", error);
+                    setLoading(false);
+                });
+        } else {
+            loadingPromise.then(() => {
+                setAleoInstance(loadedSDK);
+                setLoading(false);
+            });
+        }
     }, []);
 
     return [aleoInstance, loading];
