1c400cec82ab2e36906cb02753cfce579e72e451
Brent Conn <brent.conn@gmail.com>
2023-09-21 12:41:36 -0400

    adding root package and prettier, aleo branding

--- create-aleo-app/template-vanilla-vite/main.js
+++ create-aleo-app/template-vanilla-vite/main.js
@@ -1,5 +1,6 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
+import aleoLogo from '/aleo.svg'
 import viteLogo from '/vite.svg'
 
 const worker = new Worker("worker.js", {
@@ -8,6 +9,7 @@ const worker = new Worker("worker.js", {
 
 worker.onmessage = function(e) {
     console.log(e.data)
+    alert(e.data)
 }
 
 window.execute = () => {
@@ -23,16 +25,19 @@ document.querySelector('#app').innerHTML = `
     <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
+    <a href="https://aleo.org/" target="_blank">
+      <img src="${aleoLogo}" class="logo" alt="Aleo logo" />
+    </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <h1>Hello Aleo!</h1>
     <div class="card">
       <button onclick="window.execute()">Call Execute Function</button>
       <button onclick="window.key()">Get Private Key</button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite logo to learn more
+      Click on the Aleo logo to learn more
     </p>
   </div>
 `
