216af9d47504efd98f58c88a98f236df1d68ebbc
Pauan <pauanyu+github@pm.me>
2023-11-05 14:09:29 -0800

    Improving the optics of the justification

--- create-aleo-app/template-extension/src/service_worker.js
+++ create-aleo-app/template-extension/src/service_worker.js
@@ -13,7 +13,7 @@ async function createOffscreen(path) {
     await chrome.offscreen.createDocument({
         url: offscreenUrl,
         reasons: ["WORKERS"],
-        justification: "Top-level await and Workers cannot be used in service workers, the V3 extension system is horrendously awful.",
+        justification: "Top-level await and Workers cannot be used in service workers, but they are necessary to use the Aleo SDK.",
     });
 }
 
