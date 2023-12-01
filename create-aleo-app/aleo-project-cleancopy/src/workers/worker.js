687f8f5399a549f21bf9cd86bd17b0c2d4fd347d
frank-aleo <frank@aleo.org>
2023-12-01 14:32:07 -0800

    fixes for making deploying and executing work

--- create-aleo-app/aleo-project-cleancopy/src/workers/worker.js
+++ create-aleo-app/aleo-project-cleancopy/src/workers/worker.js
@@ -19,37 +19,18 @@ async function localProgramExecution(program, aleoFunction, inputs) {
     privateKey: "APrivateKey1zkpDH1p2331zeoQCWg7UW2E8skKWimLSxMQAM6ARBpuzMmy"
   });
 
-  // Create a new NetworkClient, KeyProvider, and RecordProvider using official Aleo record, key, and network providers
-  const networkClient = new AleoNetworkClient("https://api.explorer.aleo.org/v1");
-  const keyProvider = new AleoKeyProvider();
-  keyProvider.useCache = true;
-  const recordProvider = new NetworkRecordProvider(account, networkClient);
-  
-  // Initialize a program manager with the key provider to automatically fetch keys for executions
-  const programManager = new ProgramManager(
-    "https://api.explorer.aleo.org/v1", 
-    keyProvider, 
-    recordProvider
-  );
+  // Instantiate program manager
+  const programManager = new ProgramManager();
   programManager.setAccount(account);
 
-  console.log(program);
-  console.log(aleoFunction);
-  console.log(inputs);
-
-  // Build and execute the transaction
-  // const transaction = await programManager.buildExecutionTransaction({
-  const transaction = await programManager.run({
-    programName: "helloworld.aleo",
-    functionName: "main",
-    fee: 0.020,
-    privateFee: false,
-    inputs: ["5u32", "5u32"],
-    // keySearchParams: { "cacheKey": "helloworld:main" }
-  });
-  console.log(transaction);
-  const result = await programManager.networkClient.submitTransaction(transaction);
-  console.log(result);
+  // Execute program
+  const executionResponse = await programManager.run(
+      program,
+      aleoFunction,
+      inputs,
+      false,
+  );
+  return executionResponse.getOutputs();
 }
 
 async function getPrivateKey() {
@@ -82,8 +63,8 @@ async function deployProgram(program) {
   // Define a fee to pay to deploy the program
   const fee = 1.9; // 1.9 Aleo credits
 
-  // Deploy the program to the Aleo network
-  const tx_id = await programManager.deploy(program, fee);
+  // Deploy the program to the Aleo network using a public fee
+  const tx_id = await programManager.deploy(program, fee, false);
 
   // // Optional: Pass in fee record manually to avoid long scan times
   // const feeRecord = "{  owner: aleo1xxx...xxx.private,  microcredits: 2000000u64.private,  _nonce: 123...789group.public}";
