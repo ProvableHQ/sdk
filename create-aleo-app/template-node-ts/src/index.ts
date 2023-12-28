d978ba1a7ece69f8bde10e4606fe7430829ff7c2
WillXing <will.xing@outlook.com>
2023-12-28 15:55:23 +1300

    update localProgramExecution example in node ts template

--- create-aleo-app/template-node-ts/src/index.ts
+++ create-aleo-app/template-node-ts/src/index.ts
@@ -2,8 +2,10 @@ import {Account, initThreadPool, ProgramManager, AleoKeyProvider, AleoKeyProvide
 
 await initThreadPool();
 
+const programName = "hello_hello.aleo"
+
 const hello_hello_program =`
-program hello_hello.aleo;
+program ${programName};
 
 function hello:
     input r0 as u32.public;
@@ -11,8 +13,8 @@ function hello:
     add r0 r1 into r2;
     output r2 as u32.private;`
 
-async function localProgramExecution() {
-    const programManager = new ProgramManager(undefined, undefined, undefined);
+async function localProgramExecution(program, programName, aleoFunction, inputs) {
+    const programManager = new ProgramManager();
 
     // Create a temporary account for the execution of the program
     const account = new Account();
@@ -24,26 +26,26 @@ async function localProgramExecution() {
     programManager.setKeyProvider(keyProvider);
 
     // Pre-synthesize the program keys and then cache them in memory using key provider
-    const keyPair = await programManager.synthesizeKeys(hello_hello_program, "hello", ["1u32", "1u32"]);
+    const keyPair = await programManager.synthesizeKeys(hello_hello_program, aleoFunction, inputs);
 
     if (keyPair instanceof Error) {
         throw new Error(`Failed to synthesize keys: ${keyPair.message}`);
     } else {
-        programManager.keyProvider.cacheKeys("hello_hello.aleo:hello", keyPair);
+        programManager.keyProvider.cacheKeys(`${programName}:${aleoFunction}`, keyPair);
     }
 
-    programManager.keyProvider.cacheKeys("hello_hello.aleo:hello", keyPair);
+    programManager.keyProvider.cacheKeys(`${programName}:${aleoFunction}`, keyPair);
 
     // Specify parameters for the key provider to use search for program keys. In particular specify the cache key
     // that was used to cache the keys in the previous step.
-    const keyProviderParams = new AleoKeyProviderParams({cacheKey: "hello_hello.aleo:hello"});
+    const keyProviderParams = new AleoKeyProviderParams({cacheKey: `${programName}:${aleoFunction}`});
 
     // Execute once using the key provider params defined above. This will use the cached proving keys and make
     // execution significantly faster.
     let executionResponse = await programManager.run(
-        hello_hello_program,
-        "hello",
-        ["5u32", "5u32"],
+        program,
+        aleoFunction,
+        inputs,
         true,
         undefined,
         keyProviderParams,
@@ -60,5 +62,5 @@ async function localProgramExecution() {
 
 const start = Date.now();
 console.log("Starting execute!");
-await localProgramExecution();
+await localProgramExecution(hello_hello_program, programName, "hello", ["5u32", "5u32"]);
 console.log("Execute finished!", Date.now() - start);
