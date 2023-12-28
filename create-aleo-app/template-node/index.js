7b84328e42ca79b813fb62f61c27feefde81b0f6
WillXing <will.xing@outlook.com>
2023-12-28 15:53:26 +1300

    update localProgramExecution example in node template

--- create-aleo-app/template-node/index.js
+++ create-aleo-app/template-node/index.js
@@ -2,8 +2,10 @@ import {Account, initThreadPool, ProgramManager, AleoKeyProvider, AleoKeyProvide
 
 await initThreadPool();
 
+const programName = "hello_hello.aleo"
+
 const hello_hello_program =`
-program hello_hello.aleo;
+program ${programName};
 
 function hello:
     input r0 as u32.public;
@@ -11,7 +13,7 @@ function hello:
     add r0 r1 into r2;
     output r2 as u32.private;`
 
-async function localProgramExecution(program, aleoFunction, inputs) {
+async function localProgramExecution(program, programName, aleoFunction, inputs) {
     const programManager = new ProgramManager();
 
     // Create a temporary account for the execution of the program
@@ -24,19 +26,19 @@ async function localProgramExecution(program, aleoFunction, inputs) {
     programManager.setKeyProvider(keyProvider);
 
     // Pre-synthesize the program keys and then cache them in memory using key provider
-    const keyPair = await programManager.synthesizeKeys(hello_hello_program, "hello", ["1u32", "1u32"]);
-    programManager.keyProvider.cacheKeys("hello_hello.aleo:hello", keyPair);
+    const keyPair = await programManager.synthesizeKeys(hello_hello_program, aleoFunction, inputs);
+    programManager.keyProvider.cacheKeys(`${programName}:${aleoFunction}`, keyPair);
 
     // Specify parameters for the key provider to use search for program keys. In particular specify the cache key
     // that was used to cache the keys in the previous step.
-    const keyProviderParams = new AleoKeyProviderParams({cacheKey: "hello_hello.aleo:hello"});
+    const keyProviderParams = new AleoKeyProviderParams({cacheKey: `${programName}:${aleoFunction}`});
 
     // Execute once using the key provider params defined above. This will use the cached proving keys and make
     // execution significantly faster.
     let executionResponse = await programManager.run(
-        hello_hello_program,
-        "hello",
-        ["5u32", "5u32"],
+        program,
+        aleoFunction,
+        inputs,
         true,
         undefined,
         keyProviderParams,
@@ -53,5 +55,5 @@ async function localProgramExecution(program, aleoFunction, inputs) {
 
 const start = Date.now();
 console.log("Starting execute!");
-await localProgramExecution();
+await localProgramExecution(hello_hello_program, programName, "hello", ["5u32", "5u32"]);
 console.log("Execute finished!", Date.now() - start);
