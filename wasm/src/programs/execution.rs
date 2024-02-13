782c3369c20f568c4310e008e4a4dbd27bca1d83
makerare <97452241+makerare@users.noreply.github.com>
2024-02-13 16:21:07 +0100

    Format update.
    Signed-off-by: makerare <97452241+makerare@users.noreply.github.com>

--- wasm/src/programs/execution.rs
+++ wasm/src/programs/execution.rs
@@ -137,8 +137,14 @@ mod tests {
         let verifying_key_bytes = crate::types::native::parameters::TransferPublicVerifier::load_bytes().unwrap();
         let verifying_key = VerifyingKey::from_bytes(&verifying_key_bytes).unwrap();
         assert!(
-            verify_function_execution(&execution, &verifying_key, &Program::get_credits_program(), "transfer_public", None)
-                .unwrap()
+            verify_function_execution(
+                &execution, 
+                &verifying_key, 
+                &Program::get_credits_program(), 
+                "transfer_public", 
+                None
+            )
+            .unwrap()
         );
     }
 }*/
