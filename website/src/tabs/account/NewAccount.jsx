f1e34bf92ddad6897d9b07da836a126feff89ea8
clean99 <xff9924@gmail.com>
2024-02-26 15:41:43 +0800

    refactor: optimize loading code

--- website/src/tabs/account/NewAccount.jsx
+++ website/src/tabs/account/NewAccount.jsx
@@ -28,68 +28,68 @@ export const NewAccount = () => {
 
     const layout = { labelCol: { span: 3 }, wrapperCol: { span: 21 } };
 
-    if (aleo !== null) {
-        return (
-            <Card
-                title="Create a New Account"
-                style={{ width: "100%" }}
-            >
-                <Row justify="center">
-                    <Col>
-                        <Button
-                            type="primary"
-                            size="large"
-                            onClick={generateAccount}
-                            loading={!!loading}
-                        >
-                            Generate
-                        </Button>
-                    </Col>
-                    <Col offset="1">
-                        <Button  size="large" onClick={clear}>
-                            Clear
-                        </Button>
-                    </Col>
-                </Row>
-                {account && (
-                    <Form {...layout}>
-                        <Divider />
-                        <Form.Item label="Private Key" colon={false}>
-                            <Input
-                                size="large"
-                                placeholder="Private Key"
-                                value={privateKey()}
-                                addonAfter={<CopyButton data={privateKey()} />}
-                                disabled
-                            />
-                        </Form.Item>
-                        <Form.Item label="View Key" colon={false}>
-                            <Input
-                                size="large"
-                                placeholder="View Key"
-                                value={viewKey()}
-                                addonAfter={<CopyButton data={viewKey()} />}
-                                disabled
-                            />
-                        </Form.Item>
-                        <Form.Item label="Address" colon={false}>
-                            <Input
-                                size="large"
-                                placeholder="Address"
-                                value={address()}
-                                addonAfter={<CopyButton data={address()} />}
-                                disabled
-                            />
-                        </Form.Item>
-                    </Form>
-                )}
-            </Card>
-        );
-    } else {
+    if (!aleo) {
         return (
             <h3>
                 <center>Loading...</center>
             </h3>
         );
     }
+
+    return (
+        <Card
+            title="Create a New Account"
+            style={{ width: "100%" }}
+        >
+            <Row justify="center">
+                <Col>
+                    <Button
+                        type="primary"
+                        size="large"
+                        onClick={generateAccount}
+                        loading={!!loading}
+                    >
+                        Generate
+                    </Button>
+                </Col>
+                <Col offset="1">
+                    <Button  size="large" onClick={clear}>
+                        Clear
+                    </Button>
+                </Col>
+            </Row>
+            {account && (
+                <Form {...layout}>
+                    <Divider />
+                    <Form.Item label="Private Key" colon={false}>
+                        <Input
+                            size="large"
+                            placeholder="Private Key"
+                            value={privateKey()}
+                            addonAfter={<CopyButton data={privateKey()} />}
+                            disabled
+                        />
+                    </Form.Item>
+                    <Form.Item label="View Key" colon={false}>
+                        <Input
+                            size="large"
+                            placeholder="View Key"
+                            value={viewKey()}
+                            addonAfter={<CopyButton data={viewKey()} />}
+                            disabled
+                        />
+                    </Form.Item>
+                    <Form.Item label="Address" colon={false}>
+                        <Input
+                            size="large"
+                            placeholder="Address"
+                            value={address()}
+                            addonAfter={<CopyButton data={address()} />}
+                            disabled
+                        />
+                    </Form.Item>
+                </Form>
+            )}
+        </Card>
+    );
 };
