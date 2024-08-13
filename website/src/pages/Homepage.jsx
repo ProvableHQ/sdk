f436775b17689ee98470dc452af7a36d1891fc8d
Miche <miche@provable.com>
2024-08-13 12:35:07 -0700

    add font

--- website/src/pages/Homepage.jsx
+++ website/src/pages/Homepage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Homepage.css'; // Import the CSS file
+import './Homepage.css'; 
 
 const Homepage = () => {
   return (
