6aea6f77422ab963bb91a7c99a01ab59439473af
Pauan <pauanyu+github@pm.me>
2025-01-29 18:36:54 -0800

    Fixing fetch

--- sdk/src/polyfill/fetch.ts
+++ sdk/src/polyfill/fetch.ts
@@ -5,7 +5,9 @@ import $mime from "mime/lite";
 const oldFetch = globalThis.fetch;
 
 
-async function supportsFetch() {
+let supports: Promise<boolean> | null = null;
+
+async function checkFetch() {
     try {
         await oldFetch(new URL("file:"));
         return true;
@@ -15,33 +17,39 @@ async function supportsFetch() {
     }
 }
 
+async function supportsFetch(): Promise<boolean> {
+    if (supports === null) {
+        supports = checkFetch();
+    }
 
-if (!(await supportsFetch())) {
-    // We always polyfill fetch because Node's fetch doesn't support file URLs.
-    (globalThis.fetch as any) = async function (resource: URL | RequestInfo, options: RequestInit | undefined): Promise<Response> {
-        const request = new Request(resource, options);
-
-        const url = new URL(request.url);
-
-        if (url.protocol === "file:") {
-            const readStream = $fs.createReadStream(url);
-
-            const headers: HeadersInit = {};
-
-            const type = $mime.getType(url.pathname);
-
-            if (type) {
-                headers["Content-Type"] = type;
-            }
-
-            return new Response(readStream as any, {
-                status: 200,
-                statusText: "OK",
-                headers,
-            });
-
-        } else {
-            return await oldFetch(request);
-        }
-    };
+    return await supports;
 }
+
+
+// We always polyfill fetch because Node's fetch doesn't support file URLs.
+(globalThis.fetch as any) = async function (resource: URL | RequestInfo, options: RequestInit | undefined): Promise<Response> {
+    const request = new Request(resource, options);
+
+    const url = new URL(request.url);
+
+    if (!(await supportsFetch()) && url.protocol === "file:") {
+        const readStream = $fs.createReadStream(url);
+
+        const headers: HeadersInit = {};
+
+        const type = $mime.getType(url.pathname);
+
+        if (type) {
+            headers["Content-Type"] = type;
+        }
+
+        return new Response(readStream as any, {
+            status: 200,
+            statusText: "OK",
+            headers,
+        });
+
+    } else {
+        return await oldFetch(request);
+    }
+};
