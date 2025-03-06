574a32c17b246afbaac6010521865eef03eb5a8b
Konstantin Pandl <konstantinpandl@yahoo.de>
2025-03-06 13:09:37 +0100

    update copyright

--- wasm/src/thread_pool/mod.rs
+++ wasm/src/thread_pool/mod.rs
@@ -1,4 +1,4 @@
-// Copyright (C) 2019-2023 Aleo Systems Inc.
+// Copyright (C) 2019-2025 Provable Inc.
 // This file is part of the Aleo SDK library.
 
 // The Aleo SDK library is free software: you can redistribute it and/or modify
