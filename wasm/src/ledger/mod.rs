4bf93d14b02dc0505d0d3d7e826f3dbb777f63da
Mike Turner <mike@provable.com>
2025-03-12 18:28:02 -0500

    Update SDK license headers

--- wasm/src/ledger/mod.rs
+++ wasm/src/ledger/mod.rs
@@ -13,7 +13,6 @@
 
 // You should have received a copy of the GNU General Public License
 // along with the Provable SDK library. If not, see <https://www.gnu.org/licenses/>.
-
 pub mod transaction;
 pub use transaction::Transaction;
 
