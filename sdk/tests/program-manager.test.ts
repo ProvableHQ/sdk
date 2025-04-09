1a380c827a7a4ce08ce7bb44f8d0ca94e423c752
Mike Turner <mike@provable.com>
2025-04-09 00:17:49 -0400

    Remove rust tests and replace them with js tests to take advantage of the node polyfill

--- sdk/tests/program-manager.test.ts
+++ sdk/tests/program-manager.test.ts
@@ -7,7 +7,16 @@ import {
     statePathRecordOwnerPrivateKey,
     stateRoot
 } from "./data/account-data";
-import { Account, ExecutionResponse, OfflineQuery, ProgramManager, RecordPlaintext } from "../src/node";
+import { EXECUTION, IMPORT_1, IMPORT_2, MINT_VERIFYING_KEY, PROGRAM, SPEND_VERIFYING_KEY, SPIN_VERIFYING_KEY } from "./data/program";
+import {
+    Account,
+    ExecutionResponse,
+    ImportedPrograms, ImportedVerifyingKeys,
+    OfflineQuery,
+    ProgramManager,
+    RecordPlaintext
+} from "../src/node";
+import { verifyFunctionExecution, Execution, VerifyingKey } from "@provablehq/wasm";
 
 describe('Program Manager', () => {
     const programManager = new ProgramManager("https://api.explorer.provable.com/v1", undefined, undefined);
@@ -18,6 +27,17 @@ describe('Program Manager', () => {
             const execution_result = <ExecutionResponse>await programManager.run(helloProgram, "hello", ["5u32", "5u32"], true, undefined, undefined, undefined, undefined, undefined, undefined)
             expect(execution_result.getOutputs()[0]).equal("10u32");
             programManager.verifyExecution(execution_result);
+        });
+    });
+
+    describe('Verify execution with multiple imports', () => {
+        it.skip('Program manager should execute offline and verify the resulting proof correctly', async () => {
+            const execution = Execution.fromString(EXECUTION);
+            const verifyingKey = VerifyingKey.fromString(SPIN_VERIFYING_KEY);
+
+            const imports = <ImportedPrograms>{ "puzzle_arcade_coin_v002.aleo" : IMPORT_1, "puzzle_arcade_ticket_v002.aleo": IMPORT_2 };
+            const importedVerifyingKeys = <ImportedVerifyingKeys>{ "puzzle_arcade_coin_v002.aleo" : [["spend", SPEND_VERIFYING_KEY]], "puzzle_arcade_ticket_v002.aleo" : [["mint", MINT_VERIFYING_KEY]] };
+            expect(verifyFunctionExecution(execution, verifyingKey, PROGRAM, "spin", imports, importedVerifyingKeys)).equal(true);
         });
     });
 
