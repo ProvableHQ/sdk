f8bbc14f848d39f3b9ed7b1ae0ae376f4f184889
Roee-87 <roy@provable.com>
2025-06-24 10:31:41 -0700

    refactored decryption to use correct types

--- wasm/src/programs/decrypt.rs
+++ wasm/src/programs/decrypt.rs
@@ -17,7 +17,14 @@
 pub use super::*;
 
 use crate::{
+    Field,
+    Group,
+    Scalar,
     Transition,
+    RecordCiphertext,
+    RecordPlaintext,
+    ViewKey,
+
     log,
     native::ProgramIDNative,
     types::native::{
@@ -27,7 +34,6 @@ use crate::{
         ProcessNative,
         ProgramID,
         ProgramNative,
-        RecordCiphertext,
         VerifyingKeyNative,
     },
 };
@@ -36,48 +42,33 @@ use js_sys::{Array, Object, Reflect};
 use std::{ops::Deref, str::FromStr};
 use wasm_bindgen::{JsValue, prelude::wasm_bindgen};
 
-#[wasm_bindgen(js_name = "gnerateTvk")]
-pub(crate) fn generate_tvk(
+#[wasm_bindgen(js_name = "generateTvk")]
+pub fn generate_tvk(
     view_key: &ViewKey,
-    tpk_str: &str,
-) -> Result<String, String> {
-    let tpk = Group::<N>::from_str(tpk_str)
-        .map_err(|_| "The transaction public key string provided was invalid".to_string())?;
-    let vk_native = ViewKeyNative::from_str(*view_key.to_string())
-        .map_err(|_| "The view key string provided was invalid".to_string())?;
-    let scalar *vk_native;
-    let tvk = (tpk * scalar).to_x_coordinate();
-
-    Ok(tvk.to_string())
+    tpk: &Group,
+) -> Field {
+    tpk.scalar_multiply(&view_key.to_scalar()).to_x_coordinate()
 }
 
 #[wasm_bindgen(js_name = "generateRecordVk")]
 pub(crate) fn generate_record_vk(
     view_key: &ViewKey,
     record: &Record,
-) -> Result<Uint8Array, String> {
+) -> Group {
     let record_nonce = record.nonce();
-    let record_vk = record_nonce * **view_key;
-    // Convert the record_vk to byte_le
-    let record_vk_bytes = record_vk.to_bytes_le()
-        .map_err(|_| "Failed to convert record view key to bytes".to_string())?;
-    record_vk_bytes
+    record_nonce * **view_key
 }
 
 #[wasm_bindgen(js_name = "decryptRecordSymmetricUnchecked")]
 pub fn decrypt_record_symmetric_unchecked(
-    record_vk: Uint8Array,
+    record_vk: Group,
     record_ciphertext: RecordCiphertext,
-) -> Result<Uint8Array, String> {
-    let record_view_key = Field::from_bytes_le(&record_vk)
-        .map_err(|_| "Failed to convert record view key from bytes".to_string())?;
-    let num_randomizers = record_ciphertext.num_randomizers().
-        map_err(|_| "Failed to get the number of randomizers from the record ciphertext".to_string())?;
-    let randomizers = N::hash_many_psd8(&[N::encryption_domain(), *record_view_key], num_randomizers);
+) -> Result<RecordPlaintext, String> {
+    let num_randomizers = record_ciphertext.num_randomizers()
+        .map_err(|_| "Failed to get the number of randomizers from the record ciphertext".to_string())?;
+    let randomizers = N::hash_many_psd8(&[N::encryption_domain(), *record_view_key], num_randomizers); // Not qwuite sure how to implement this on the SDK side.
     
     let record_plaintext = record_ciphertext.decrypt_with_randomizers(&randomizers);
-    let record_plaintext_bytes = record_plaintext.to_bytes_le()
-        .map_err(|_| "Failed to convert record plaintext to bytes".to_string())?;
     
-    Ok(record_plaintext_bytes)
+    Ok(record_plaintext)
 }
