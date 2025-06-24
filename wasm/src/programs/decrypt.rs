b54e33b819a796b40e168aec909e923ca3256c4d
Roee-87 <roy@provable.com>
2025-06-23 17:28:40 -0700

    added record decryption

--- wasm/src/programs/decrypt.rs
+++ wasm/src/programs/decrypt.rs
@@ -27,10 +27,10 @@ use crate::{
         ProcessNative,
         ProgramID,
         ProgramNative,
+        RecordCiphertext,
         VerifyingKeyNative,
     },
 };
-use snarkvm_algorithms::snark::varuna::VarunaVersion;
 
 use js_sys::{Array, Object, Reflect};
 use std::{ops::Deref, str::FromStr};
@@ -48,4 +48,33 @@ pub(crate) fn generate_tvk(
     let tvk = (tpk * scalar).to_x_coordinate();
 
     Ok(tvk.to_string())
+}
+
+pub(crate) fn generate_record_vk(
+    view_key: &ViewKey,
+    record: &Record,
+) -> Result<Uint8Array, String> {
+    let record_nonce = record.nonce();
+    let record_vk = record_nonce * **view_key;
+    // Convert the record_vk to byte_le
+    let record_vk_bytes = record_vk.to_bytes_le()
+        .map_err(|_| "Failed to convert record view key to bytes".to_string())?;
+    record_vk_bytes
+}
+
+pub fn decrypt_record_symmetric_unchecked(
+    record_vk: Uint8Array,
+    record_ciphertext: RecordCiphertext,
+) -> Result<Uint8Array, String> {
+    let record_view_key = Field::from_bytes_le(&record_vk)
+        .map_err(|_| "Failed to convert record view key from bytes".to_string())?;
+    let num_randomizers = record_ciphertext.num_randomizers().
+        map_err(|_| "Failed to get the number of randomizers from the record ciphertext".to_string())?;
+    let randomizers = N::hash_many_psd8(&[N::encryption_domain(), *record_view_key], num_randomizers);
+    
+    let record_plaintext = record_ciphertext.decrypt_with_randomizers(&randomizers);
+    let record_plaintext_bytes = record_plaintext.to_bytes_le()
+        .map_err(|_| "Failed to convert record plaintext to bytes".to_string())?;
+    
+    Ok(record_plaintext_bytes)
 }
