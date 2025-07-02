3fab50286998f83b0160ee59fd0de33ab1e8b416
Roee-87 <roy@provable.com>
2025-07-02 12:16:32 -0700

    fixed imports, CONST vars ordering, and punctuation

--- wasm/src/record/record_ciphertext.rs
+++ wasm/src/record/record_ciphertext.rs
@@ -23,10 +23,10 @@ use crate::{
     js_array_from_fields,
     to_bits_array_le,
     types::native::{CurrentNetwork, RecordCiphertextNative},
+    utilities::encrypt::EncryptionToolkilt,
 };
 use snarkvm_console::prelude::{FromBytes, Network, ToBits, ToBytes, ToFields};
 
-use crate::utilities::encrypt::EncryptionToolkit;
 use js_sys::{Array, Uint8Array};
 use std::{ops::Deref, str::FromStr};
 use wasm_bindgen::prelude::*;
@@ -131,7 +131,7 @@ impl RecordCiphertext {
         Ok(js_array_from_fields!(&native_fields))
     }
 
-    /// Decrypt the record ciphertext into plaintext using a record view key
+    /// Decrypt the record ciphertext into plaintext using a record view key.
     ///
     /// @param {Field} record_vk Record view key used to decrypt the record.
     ///
@@ -196,16 +196,16 @@ mod tests {
 
     use wasm_bindgen_test::wasm_bindgen_test;
 
-    const OWNER_PLAINTEXT: &str = r"{
-  owner: aleo1j7qxyunfldj2lp8hsvy7mw5k8zaqgjfyr72x2gh3x4ewgae8v5gscf5jh3.private,
-  microcredits: 1500000000000000u64.private,
-  _nonce: 3077450429259593211617823051143573281856129402760267155982965992208217472983group.public
-}";
-    const OWNER_CIPHERTEXT: &str = "record1qyqsqpe2szk2wwwq56akkwx586hkndl3r8vzdwve32lm7elvphh37rsyqyxx66trwfhkxun9v35hguerqqpqzqrtjzeu6vah9x2me2exkgege824sd8x2379scspmrmtvczs0d93qttl7y92ga0k0rsexu409hu3vlehe3yxjhmey3frh2z5pxm5cmxsv4un97q";
-    const OWNER_VIEW_KEY: &str = "AViewKey1ccEt8A2Ryva5rxnKcAbn7wgTaTsb79tzkKHFpeKsm9NX";
-    const RECORD_VIEW_KEY: &str = "4445718830394614891114647247073357094867447866913203502139893824059966201724field";
     const NON_OWNER_VIEW_KEY: &str = "AViewKey1e2WyreaH5H4RBcioLL2GnxvHk5Ud46EtwycnhTdXLmXp";
+    const OWNER_CIPHERTEXT: &str = "record1qyqsqpe2szk2wwwq56akkwx586hkndl3r8vzdwve32lm7elvphh37rsyqyxx66trwfhkxun9v35hguerqqpqzqrtjzeu6vah9x2me2exkgege824sd8x2379scspmrmtvczs0d93qttl7y92ga0k0rsexu409hu3vlehe3yxjhmey3frh2z5pxm5cmxsv4un97q";
+    const OWNER_PLAINTEXT: &str = r"{
+        owner: aleo1j7qxyunfldj2lp8hsvy7mw5k8zaqgjfyr72x2gh3x4ewgae8v5gscf5jh3.private,
+        microcredits: 1500000000000000u64.private,
+        _nonce: 3077450429259593211617823051143573281856129402760267155982965992208217472983group.public
+      }";
+    const OWNER_VIEW_KEY: &str = "AViewKey1ccEt8A2Ryva5rxnKcAbn7wgTaTsb79tzkKHFpeKsm9NX";
     const RECORD_TAG: &str = "1796466189545157638691489609907096471289658804813960182690905095269699169603field";
+    const RECORD_VIEW_KEY: &str = "4445718830394614891114647247073357094867447866913203502139893824059966201724field";
 
     // Related material for use in future tests
     const _OWNER_PRIVATE_KEY: &str = "APrivateKey1zkpJkyYRGYtkeHDaFfwsKtUJzia7csiWhfBWPXWhXJzy9Ls";
