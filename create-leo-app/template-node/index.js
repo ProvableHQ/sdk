37749a67e0be69af37000d0f5ad0156ab97caacd
Roee-87 <roy@provable.com>
2025-08-26 16:46:45 -0700

    fixed template file

--- create-leo-app/template-node/index.js
+++ create-leo-app/template-node/index.js
@@ -1,10 +1,19 @@
-import {Account, initThreadPool, ProgramManager, AleoKeyProvider, OfflineKeyProvider, CREDITS_PROGRAM_KEYS} from "@provablehq/sdk";
+import {Account, initThreadPool, ProgramManager, AleoKeyProvider, AleoKeyProviderParams} from "@provablehq/sdk";
 
 await initThreadPool();
 
+const programName = "hello_hello.aleo"
 
+const hello_hello_program =`
+program ${programName};
 
-async function localProgramExecution() {
+function hello:
+    input r0 as u32.public;
+    input r1 as u32.private;
+    add r0 r1 into r2;
+    output r2 as u32.private;`
+
+async function localProgramExecution(program, programName, aleoFunction, inputs) {
     const programManager = new ProgramManager();
 
     // Create a temporary account for the execution of the program
@@ -13,19 +22,38 @@ async function localProgramExecution() {
 
     // Create a key provider in order to re-use the same key for each execution
     const keyProvider = new AleoKeyProvider();
-    const offlineKeyProvider = new OfflineKeyProvider();
     keyProvider.useCache(true);
     programManager.setKeyProvider(keyProvider);
 
-     const [transferPublicProver, transferPublicVerifier] =await keyProvider.fetchCreditsKeys(CREDITS_PROGRAM_KEYS.transfer_public);
-    console.log(transferPublicVerifier.toString());
+    // Pre-synthesize the program keys and then cache them in memory using key provider
+    const keyPair = await programManager.synthesizeKeys(hello_hello_program, aleoFunction, inputs);
+    programManager.keyProvider.cacheKeys(`${programName}:${aleoFunction}`, keyPair);
 
     // Specify parameters for the key provider to use search for program keys. In particular specify the cache key
     // that was used to cache the keys in the previous step.
-   
+    const keyProviderParams = new AleoKeyProviderParams({cacheKey: `${programName}:${aleoFunction}`});
+
+    // Execute once using the key provider params defined above. This will use the cached proving keys and make
+    // execution significantly faster.
+    let executionResponse = await programManager.run(
+        program,
+        aleoFunction,
+        inputs,
+        true,
+        undefined,
+        keyProviderParams,
+    );
+    console.log("hello_hello/hello executed - result:", executionResponse.getOutputs());
+
+    // Verify the execution using the verifying key that was generated earlier.
+    if (programManager.verifyExecution(executionResponse, 9_000_000)) {
+        console.log("hello_hello/hello execution verified!");
+    } else {
+        throw("Execution failed verification!");
+    }
 }
 
 const start = Date.now();
 console.log("Starting execute!");
-await localProgramExecution();
+await localProgramExecution(hello_hello_program, programName, "hello", ["5u32", "5u32"]);
 console.log("Execute finished!", Date.now() - start);
