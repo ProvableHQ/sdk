bd070d4a566026e3e02656a4ff71c150b1459291
Michael Turner <mike@provable.com>
2025-10-15 15:47:16 -0400

    Update tests to use API V1

--- wasm/src/utilities/rest.rs
+++ wasm/src/utilities/rest.rs
@@ -96,7 +96,7 @@ mod tests {
                 )
                 .unwrap(),
             ];
-            let state_paths = get_statepaths_for_commitments("http://34.168.156.3:3030", &commitments).await.unwrap();
+            let state_paths = get_statepaths_for_commitments("https://api.explorer.provable.com/v1", &commitments).await.unwrap();
             assert_eq!(state_paths.len(), 2);
             assert_eq!(state_paths[0].global_state_root(), state_paths[1].global_state_root());
         }
