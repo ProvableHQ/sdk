a0e1a64da47cfa5624c9460c10b3b1acc90a4bcb
Mike Turner <mike@provable.com>
2025-12-07 21:56:53 -0500

    Refactor method signatures to use consistent spacing
    Signed-off-by: Mike Turner <mike@provable.com>

--- sdk/src/keys/keystore/keystore.ts
+++ sdk/src/keys/keystore/keystore.ts
@@ -9,7 +9,7 @@ export interface KeyStore {
      *
      * @returns {Promise<FunctionKeyPair | null>} Returns the proving and verifying keys for the given locator if they exist or null if they do not.
      */
-    getKeys(locator:string): Promise<FunctionKeyPair | null>
+    getKeys(locator: string): Promise<FunctionKeyPair | null>;
 
     /**
      * Returns the proving and verifying key as raw bytes for a given key locator from key storage.
@@ -17,7 +17,7 @@ export interface KeyStore {
      * @param {string} locator The unique locator for the desired keypair.
      *
      * @returns {Promise<CachedKeyPair | null>} Returns the proving and verifying keys for the given locator as raw bytes if they exist or null if they do not.     */
-    getKeysRaw(locator:string): Promise<CachedKeyPair | null>
+    getKeysRaw(locator: string): Promise<CachedKeyPair | null>;
 
     /**
      * Returns the `ProvingKey` for a given locator.
@@ -26,7 +26,7 @@ export interface KeyStore {
      *
      * @returns {Promise<ProvingKey | null>} Returns the `ProvingKey` for the given locator if it exists or null if it does not.
      */
-    getProvingKey(locator:string): Promise<ProvingKey | null>
+    getProvingKey(locator: string): Promise<ProvingKey | null>;
 
     /**
      * Returns the raw bytes of a `ProvingKey` for a given locator.
@@ -35,7 +35,7 @@ export interface KeyStore {
      *
      * @returns {Promise<Uint8Array | null>} Returns the raw bytes of a `ProvingKey` for the given locator if it exists or null if it does not exist.
      */
-    getProvingKeyRaw(locator:string): Promise<Uint8Array | null>
+    getProvingKeyRaw(locator: string): Promise<Uint8Array | null>;
 
     /**
      * Returns the `VerifyingKey` for a given locator.
@@ -44,7 +44,7 @@ export interface KeyStore {
      *
      * @returns {Promise<VerifyingKey | null>} Returns the `VerifyingKey` for the given locator if it exists or null if it does not exist.
      */
-    getVerifyingKey(locator:string): Promise<VerifyingKey | null>
+    getVerifyingKey(locator: string): Promise<VerifyingKey | null>;
 
     /**
      * Returns the raw bytes of a `VerifyingKey` for a given locator.
@@ -53,7 +53,7 @@ export interface KeyStore {
      *
      * @returns {Promise<Uint8Array | null>} Returns the raw bytes of a `VerifyingKey` for the given locator if it exists or null if it does not exist.
      */
-    getVerifyingKeyRaw(locator:string): Promise<Uint8Array | null>
+    getVerifyingKeyRaw(locator: string): Promise<Uint8Array | null>;
 
     /**
      * Stores proving and verifying keys in key storage.
@@ -61,7 +61,7 @@ export interface KeyStore {
      * @param {string} locator The unique locator for the desired keypair.
      * @param {FunctionKeyPair} keys The proving and verifying keys.
      */
-    setKeys(locator:string, keys: FunctionKeyPair): Promise<void>
+    setKeys(locator: string, keys: FunctionKeyPair): Promise<void>;
 
     /**
      * Stores the raw proving and verifying key bytes in key storage.
@@ -69,24 +69,24 @@ export interface KeyStore {
      * @param {string} locator The unique locator for the desired keypair.
      * @param {CachedKeyPair} keys The raw proving and verifying key bytes.
      */
-    setKeysRaw(locator:string, keys: CachedKeyPair): Promise<void>
+    setKeysRaw(locator: string, keys: CachedKeyPair): Promise<void>;
 
     /**
      * Determines if a given keypair exists or not.
      *
      * @param {string} locator The unique locator for the desired keypair.
      */
-    has(locator:string): Promise<boolean>
+    has(locator: string): Promise<boolean>;
 
     /**
      * Deletes a keypair corresponding to a given locator.
      *
      * @param {string} locator The unique locator for the desired keypair.
      */
-    delete(locator:string): Promise<void>
+    delete(locator: string): Promise<void>;
 
     /**
      * Clears all keys in the keystore.
      */
-    clear(): Promise<void>
+    clear(): Promise<void>;
 }
