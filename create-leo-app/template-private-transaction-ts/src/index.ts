71d052161565be6c3e0241609f426b979e3b5c6d
Michael Turner <mike@provable.com>
2026-01-15 12:49:28 -0500

    Update Cargo.toml to snarkvm version 4.4.0 and edit template-private-transaction-ts to use xmlhttprequest downloads by default to test in ci

--- create-leo-app/template-private-transaction-ts/src/index.ts
+++ create-leo-app/template-private-transaction-ts/src/index.ts
@@ -22,10 +22,15 @@ programManager.setAccount(account);
 const keyProvider = new AleoKeyProvider();
 keyProvider.useCache(true);
 
-// Initialize the keyProvider cache with all necessary keys.
+// ----- Uncomment these line for faster proving! -------
+// This line set fetches the required keys for proving and stores them in the key provider.
 // await Promise.all([keyProvider.transferKeys("private"), keyProvider.feePrivateKeys()]);
-programManager.setKeyProvider(keyProvider);
+// This line sets the inclusion prover which is required for transactions that use records in order to prove their
+// inclusion in aleo history.
 // await programManager.setInclusionProver();
+
+// Initialize the keyProvider cache with all necessary keys.
+programManager.setKeyProvider(keyProvider);
 
 const start = Date.now();
 console.log("Starting transfer_private execution");
