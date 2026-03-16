ae449a9bf20a653d3c1bb6d5bfd05e6ee618c85c
Roee-87 <roy@provable.com>
2026-03-16 16:41:50 -0700

    updated reac-managed-worker template

--- create-leo-app/template-react-managed-worker/vite.config.js
+++ create-leo-app/template-react-managed-worker/vite.config.js
@@ -1,21 +1,31 @@
-import { defineConfig, searchForWorkspaceRoot } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  assetsInclude: ['**/*.wasm'],
+  assetsInclude: ["**/*.wasm"],
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ["@provablehq/wasm"],
+
+  build: {
+    target: "esnext",
   },
-  server: {
-    // Needed if you are linking local packages for development
-    fs: {
-      allow: [searchForWorkspaceRoot(process.cwd()), "../../sdk"],
+
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "esnext",
     },
+    exclude: [
+      "@provablehq/wasm",
+      "@provablehq/wasm-account-tools",
+    ],
+  },
+
+  server: {
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin",
       "Cross-Origin-Embedder-Policy": "require-corp",
     },
+  },
+  worker: {
+    format: "es",
   },
 });
