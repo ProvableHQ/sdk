d794e214c54ffa49cb536dd8ebcdb18a0c823d4b
Roee-87 <roy@provable.com>
2026-04-28 11:57:45 -0700

    added check for usdcx_stablecoin.aleo

--- sdk/tests/program-manager.test.ts
+++ sdk/tests/program-manager.test.ts
@@ -34,7 +34,7 @@ import {
     statePathv0RecordOwnerPrivateKey,
     stateRootv0,
 } from "./data/account-data.js";
-import { IMPORT_1, IMPORT_2, MINT_VERIFYING_KEY, PROGRAM, SPEND_VERIFYING_KEY, SPIN_VERIFYING_KEY } from "./data/program.js";
+import { IMPORT_1, IMPORT_2, MINT_VERIFYING_KEY, PROGRAM, SPEND_VERIFYING_KEY, SPIN_VERIFYING_KEY, USDCX_STABLECOIN_PROGRAM } from "./data/program.js";
 import { RECORD_PLAINTEXT_V1_STRING } from "./data/records";
 import { SAMPLE_VERIFYING_KEY, SAMPLE_PROOF, SAMPLE_INPUTS } from "./data/snark-verify.js";
 import { expect } from "chai";
@@ -591,6 +591,13 @@ describe('Program Manager', async () => {
             const checksum = program.toChecksum();
             // keccak256 of Program.fromString(IMPORT_1).toString() as UTF-8 bytes
             const expected = [117, 142, 190, 189, 36, 240, 186, 228, 175, 186, 39, 69, 170, 179, 204, 38, 100, 107, 241, 94, 88, 233, 58, 240, 219, 147, 141, 94, 157, 128, 199, 157];
+            expect(checksum).to.deep.equal(expected);
+        });
+
+        it('should produce the correct checksum for usdcx_stablecoin.aleo', () => {
+            const program = Program.fromString(USDCX_STABLECOIN_PROGRAM);
+            const checksum = program.toChecksum();
+            const expected = [194, 65, 103, 185, 169, 136, 248, 193, 48, 73, 60, 67, 15, 90, 173, 101, 12, 168, 11, 167, 205, 1, 44, 89, 28, 211, 107, 83, 92, 196, 57, 114];
             expect(checksum).to.deep.equal(expected);
         });
     });
