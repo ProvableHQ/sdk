0cffae8ac2c7e7133afe14fecc29c3d1138057ad
Michael Turner <mike@provable.com>
2026-06-28 22:30:42 -0500

    Update consensus version test & docstrings

--- sdk/tests/wasm.test.ts
+++ sdk/tests/wasm.test.ts
@@ -627,7 +627,7 @@ describe('WASM Objects', () => {
         });
     });
     describe('Set development consensus version heights', () => {
-        it.only('Consensus version heights can be set externally', async () => {
+        it('Consensus version heights can be set externally', async () => {
             if (process.env["RUN_SKIPPED"]) {
                 const heights = getOrInitConsensusVersionTestHeights("0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16");
                 console.log(heights);
